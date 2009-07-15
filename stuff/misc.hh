ec756dce525858a031e4f9d6f226e45f85949e84
Rene Milk <r_milk01@DUNE-PROJECT.uni-muenster.de>
2009-07-15 17:58:47 +0200

    enable to put logs in own dir
    Signed-off-by: Rene Milk <r_milk01@DUNE-PROJECT.uni-muenster.de>

--- stuff/misc.hh
+++ stuff/misc.hh
@@ -180,7 +180,7 @@ public:
  **/
 
 template <class T>
-void safe_delete(T t)
+void safe_delete(T t) // this is actually bullshit :P
 {
   if (t) {
     delete t;
@@ -198,6 +198,49 @@ int getIdx(const Container& ct, Element e)
       return idx;
   }
   return -1;
+}
+
+//! strip filename from \path if present, return empty string if only filename present
+std::string pathOnly(std::string path)
+{
+  if (!path.empty()) {
+    char buf[1024]; // not _exactly_ sure this is max path length, but it's suggested in wx source
+
+    // Local copy
+    strcpy(buf, path.c_str());
+
+    int l = path.length();
+    int i = l - 1;
+
+    // Search backward for a backward or forward slash
+    while (i > -1) {
+      if ((path[i] == '/') || (path[i] == '\\')) {
+        // Don't return an empty string
+        if (i == 0)
+          i++;
+        buf[i] = 0;
+        return std::string(buf);
+      }
+      i--;
+    }
+  }
+  return std::string();
+}
+
+//! may include filename, will be stripped
+bool testCreateDirectory(std::string path)
+{
+  std::string pathonly = pathOnly(path);
+  if (pathonly.empty())
+    return true; // no dir to create
+
+  // maybe test if dir exists??
+  bool ok = (mkdir(pathonly.c_str(), 0755) == 0);
+  if (!ok) {
+    perror(pathonly.c_str());
+    return errno == EEXIST;
+  }
+  return true;
 }
 
 } // end namepspace stuff
