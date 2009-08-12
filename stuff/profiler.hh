f3031cfcbaa0333f6ef5de606efb19fcda7d0fdb
Rene Milk <koshi@springlobby.info>
2009-08-12 18:42:47 +0200

    revamp profiler to be usable in multiple, not meant for averaging, runs

--- stuff/profiler.hh
+++ stuff/profiler.hh
@@ -46,23 +46,31 @@ protected:
 
 public:
   //! set this to begin a named section
-  inline void StartTiming(const std::string section_name)
+  void StartTiming(const std::string section_name)
   {
     TimingData td(section_name, clock());
     (m_timings[m_cur_run_num])[section_name] = td;
   }
 
   //! stop named section's counter
-  inline void StopTiming(const std::string section_name)
+  void StopTiming(const std::string section_name)
   {
     (m_timings[m_cur_run_num])[section_name].end = clock();
   }
 
-  inline long GetTiming(const std::string section_name)
+  long GetTiming(const std::string section_name)
   {
     clock_t diff = (m_timings[m_cur_run_num])[section_name].end - (m_timings[m_cur_run_num])[section_name].start;
     return long(diff / double(CLOCKS_PER_SEC));
   }
+
+  /** output to currently pre-defined (csv) file, do not put individual run results, but average over all recorded
+   *results
+   * \param comm used to gather and average the runtime data over all processes
+   * \tparam CollectiveCommunication should be Dune::CollectiveCommunication< MPI_Comm / double >
+   **/
+  template <class CollectiveCommunication>
+  long OutputAveraged(CollectiveCommunication& comm, const int refineLevel, const long numDofs);
 
   /** output to currently pre-defined (csv) file
    * \param comm used to gather and average the runtime data over all processes
@@ -75,31 +83,31 @@ public:
    *  if you're planning on doing more than one iteration of your code
    *  called once fromm ctor with numRuns=1
    **/
-  inline void Reset(const int numRuns)
+  void Reset(const int numRuns)
   {
     m_timings.clear();
-    // m_timings.reserve( numRuns*2 );
+    m_timings     = MapVector(numRuns, DataMap());
     m_total_runs  = numRuns;
     m_cur_run_num = 0;
-    DataMap td;
-    m_timings.push_back(td);
+    //			DataMap td;
+    //            m_timings.push_back( td );
     m_l2_error = 0;
     init_time_ = clock();
   }
 
   //! simple counter, usable to count how often a single piece of code is called
-  inline void AddCount(const int num)
+  void AddCount(const int num)
   {
     m_count[num] += 1;
   }
 
   //! call this after one iteration of your code has finished. increments current run number and puts new timing data
   //! into the vector
-  inline void NextRun(const double error)
+  void NextRun(const double error)
   {
-    m_cur_run_num += m_cur_run_num < m_total_runs - 1 ? 1 : 0;
-    DataMap td;
-    m_timings.push_back(td);
+    m_cur_run_num++; // +=  m_cur_run_num < m_total_runs - 1 ?  1 : 0 ;
+    //            DataMap td;
+    //            m_timings.push_back( td );
     m_l2_error += error;
   }
 
@@ -114,7 +122,7 @@ protected:
 };
 
 template <class CollectiveCommunication>
-long Profiler::Output(CollectiveCommunication& comm, const int refineLevel, const long numDofs)
+long Profiler::OutputAveraged(CollectiveCommunication& comm, const int refineLevel, const long numDofs)
 {
   const int numProce = comm.size();
 
@@ -144,27 +152,79 @@ long Profiler::Output(CollectiveCommunication& comm, const int refineLevel, cons
   }
 
   // outputs column names
-  csv << "refine;"
-      << "processes; "
-      << "numDofs; "
-      << "L1 error; ";
+  csv << "refine,"
+      << "processes,"
+      << "numDofs,"
+      << "L1 error,";
   for (AvgMap::const_iterator it = averages.begin(); it != averages.end(); ++it) {
-    csv << it->first << ";";
+    csv << it->first << ",";
   }
-  csv << "Speedup (total); Speedup (ohne Solver)";
+  csv << "Speedup (total); Speedup (ohne Solver)" << std::endl;
 
   // outputs column values
-  csv << std::endl
-      << refineLevel << ";" << comm.size() << ";" << numDofs << ";"
-      << comm.sum(m_l2_error) / double(m_total_runs * numProce) << ";";
+  csv << refineLevel << "," << comm.size() << "," << numDofs << ","
+      << comm.sum(m_l2_error) / double(m_total_runs * numProce) << ",";
   for (AvgMap::const_iterator it = averages.begin(); it != averages.end(); ++it) {
     long clock_count = it->second;
     clock_count = long(comm.sum(clock_count) / double(CLOCKS_PER_SEC * 0.001 * numProce));
-    csv << clock_count / double(m_total_runs) << ";";
+    csv << clock_count / double(m_total_runs) << ",";
   }
-  csv << "=I$2/I2;"
+  csv << "=I$2/I2,"
       << "=SUM(E$2:G$2)/SUM(E2:G2)" << std::endl;
 
+
+  csv.close();
+
+  return long((clock() - init_time_) / double(CLOCKS_PER_SEC * 0.001));
+}
+
+template <class CollectiveCommunication>
+long Profiler::Output(CollectiveCommunication& comm, const int refineLevel, const long numDofs)
+{
+  const int numProce = comm.size();
+
+  std::ostringstream filename;
+  filename << "p" << numProce << "_refinelvl_" << refineLevel << ".csv";
+  filename.flush();
+
+  if (comm.rank() == 0)
+    std::cout << "Profiling info in: " << (filename.str()).c_str() << std::endl;
+
+#ifndef NDEBUG
+  for (std::map<int, int>::const_iterator it = m_count.begin(); it != m_count.end(); ++it) {
+    std::cout << "proc " << comm.rank() << " bId " << it->first << " count " << it->second << std::endl;
+  }
+#endif
+
+  std::ofstream csv((filename.str()).c_str());
+
+  // outputs column names
+  csv << "refine,"
+      << "processes,"
+      << "numDofs,"
+      << "L2 error,";
+  for (DataMap::const_iterator it = m_timings[0].begin(); it != m_timings[0].end(); ++it) {
+    csv << it->first << ",";
+  }
+  csv << "Speedup (total); Speedup (ohne Solver)" << std::endl;
+
+  // outputs column values
+  csv << refineLevel << "," << comm.size() << "," << numDofs << ","
+      << comm.sum(m_l2_error) / double(m_total_runs * numProce) << ",";
+
+  MapVector::const_iterator ti_it = m_timings.begin();
+  for (; ti_it != m_timings.end(); ++ti_it) {
+
+    const DataMap& data_map = *ti_it;
+    for (DataMap::const_iterator it = data_map.begin(); it != data_map.end(); ++it) {
+      TimingData data  = it->second;
+      long clock_count = data.end - data.start;
+      clock_count = long(comm.sum(clock_count) / double(CLOCKS_PER_SEC * 0.001 * numProce));
+      csv << clock_count / double(m_total_runs) << ",";
+    }
+    csv << "=I$2/I2,"
+        << "=SUM(E$2:G$2)/SUM(E2:G2)" << std::endl;
+  }
 
   csv.close();
 
