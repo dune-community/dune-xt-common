ca971ddf8c2e15460c0d1ec9eb99d0139fd6a0d2
Rene Milk <rene.milk@uni-muenster.de>
2010-09-28 02:49:36 +0200

    missing includes fixed

--- stuff/misc.hh
+++ stuff/misc.hh
@@ -154,6 +154,7 @@ typedef unsigned int RunInfoVectorMapKeyType;
 
 typedef std::map<RunInfoVectorMapKeyType, RunInfoVector> RunInfoVectorMap;
 
+#include <cstdio>
 #include <dune/common/misc.hh>
 
 namespace Stuff {
