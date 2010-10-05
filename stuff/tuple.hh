5103295e191af97f53559600fd55905742884e5b
Rene Milk <rene.milk@uni-muenster.de>
2010-10-05 21:17:44 +0200

    new tuple class

--- stuff/tuple.hh
+++ stuff/tuple.hh
@@ -1,6 +1,8 @@
 #ifndef DUNE_STUFF_TUPLE_HH
 #define DUNE_STUFF_TUPLE_HH
 
+#include <dune/common/tuples.hh>
+
 namespace Stuff {
 
 //! split our function wrapper into sep pointers for Datawriter input
@@ -59,6 +61,16 @@ struct TupleSerializer
     TupleType* t =
         new TupleType(&(t1.discreteVelocity()), &(t1.discretePressure()), NULL, NULL, NULL, NULL, NULL, NULL);
     return *t;
+  }
+};
+
+template <class T1, class T2 = T1, class T3 = T1, class T4 = T1, class T5 = T1, class T6 = T1, class T7 = T1,
+          class T8 = T1, class T9 = T1>
+struct FullTuple : public Dune::Tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9>
+{
+  FullTuple(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9)
+    : Dune::Tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9>(t1, t2, t3, t4, t5, t6, t7, t8, t9)
+  {
   }
 };
 
