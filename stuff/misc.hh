7950aaf73925b7b933790abdb31ae6f554cf08ce
Rene Milk <rene.milk@uni-muenster.de>
2010-12-01 18:47:12 +0100

    timeseries output includes alog name

--- stuff/misc.hh
+++ stuff/misc.hh
@@ -136,6 +136,7 @@ struct RunInfo
   double max_inner_accuracy;
   std::string problemIdentifier;
   double current_time, delta_t, viscosity, reynolds, alpha;
+  std::string algo_id;
 
   RunInfo()
   {
@@ -148,6 +149,7 @@ struct RunInfo
     delta_t      = 0.1;
     current_time = 0.0;
     viscosity = reynolds = 1.0;
+    algo_id = "N.A.";
   }
 };
 
