447be528f92b8b4e1796fda32cf217656c58a437
Rene Milk <rene.milk@uni-muenster.de>
2011-01-13 02:26:39 +0100

    useless matrix transpose

--- stuff/matrix.hh
+++ stuff/matrix.hh
@@ -300,6 +300,17 @@ public:
     }
   }
 };
+
+template <class M>
+void forceTranspose(const M& arg, M& dest)
+{
+  assert(arg.cols() == dest.rows());
+  assert(dest.cols() == arg.rows());
+  //			dest.clear();
+  for (int i = 0; i < arg.cols(); ++i)
+    for (int j = 0; j < arg.rows(); ++j)
+      dest.set(j, i, arg(i, j));
+}
 }
 } // namespace Stuff
 
