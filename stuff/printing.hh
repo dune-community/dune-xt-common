6b1bdad637e017e80f513fd5ee1e7742673097d7
Rene Milk <rene.milk@uni-muenster.de>
2011-12-18 22:07:58 +0100

    temp dsiable somematrix stuff

--- stuff/printing.hh
+++ stuff/printing.hh
@@ -341,7 +341,8 @@ void matrixToGnuplotStream(const Matrix& matrix, Stream& stream)
   for (size_t row = 0; row < matrix.rows(); ++row) {
     for (size_t col = 0; col < matrix.cols(); ++col) {
       if (matrix.find(row, col))
-        stream << row << "\t" << col << "\t" << matrix(row, col) << std::endl;
+        assert(false);
+      //                stream << row << "\t" << col << "\t" << matrix( row, col ) << std::endl;
     }
     nz += matrix.numNonZeros(row);
     stream << "#non zeros in row " << row << " " << matrix.numNonZeros(row) << " (of " << matrix.cols() << " cols)\n";
