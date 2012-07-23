9d6fc189c12f4a9461f98e037a67da20ca212b89
Rene Milk <rene.milk@uni-muenster.de>
2012-07-23 22:41:37 +0200

    [tests] adds a bunch of new test skeletons

---
+++ dune/stuff/test/common_string.cc
@@ -0,0 +1,18 @@
+#ifdef HAVE_CMAKE_CONFIG
+#include "cmake_config.h"
+#elif defined(HAVE_CONFIG_H)
+#include <config.h>
+#endif // ifdef HAVE_CMAKE_CONFIG
+
+int main(int argc, char** argv)
+{
+  try {
+    // mpi
+    Dune::MPIHelper::instance(argc, argv);
+
+  } catch (Dune::Exception& e) {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
+}
