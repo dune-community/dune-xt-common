55f416bf5ba8132ea8b26203a865a6d617524103
Stefan Girke <stefan.girke@gmx.de>
2012-11-16 16:18:00 +0100

    bug-fix

--- dune/stuff/common/tuple.hh
+++ dune/stuff/common/tuple.hh
@@ -4,72 +4,81 @@
 #include <dune/common/tuples.hh>
 #include <dune/common/typetraits.hh>
 
+#define TMAX(t_, no_) (tuple_size<t_>::value >= (no_ + 1) ? no_ : 0)
+#define TELE(t_, s_, no_) typename Dune::tuple_element<TMAX(t_, no_), t_>::type::s_
 
-struct NoElement
-{
-};
-#define TMAX(t_, no_) (Dune::tuple_size<t_>::value <= (no_ + 1) ? Dune::tuple_size<t_>::value : no_)
-#define TELE(t_, s_, no_)                                                                                              \
-  typename Dune::tuple_element<TMAX(t_, no_), typename Dune::PushFrontTuple<t_, NoElement>::type>::type
-#define TCOND(t_, no_) (Dune::tuple_size<t_>::value <= no_)
-
-template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8>
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, int Size>
 struct RightTrimTuple
+{
+  typedef Dune::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9> type;
+};
+
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 9>
+{
+  typedef Dune::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9> type;
+};
+
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 8>
 {
   typedef Dune::tuple<T1, T2, T3, T4, T5, T6, T7, T8> type;
 };
 
-template <class T1>
-struct RightTrimTuple<T1, NoElement, NoElement, NoElement, NoElement, NoElement, NoElement, NoElement>
-{
-  typedef Dune::tuple<T1> type;
-};
-
-template <class T1, class T2>
-struct RightTrimTuple<T1, T2, NoElement, NoElement, NoElement, NoElement, NoElement, NoElement>
-{
-  typedef Dune::tuple<T1, T2> type;
-};
-
-template <class T1, class T2, class T3>
-struct RightTrimTuple<T1, T2, T3, NoElement, NoElement, NoElement, NoElement, NoElement>
-{
-  typedef Dune::tuple<T1, T2, T3> type;
-};
-
-template <class T1, class T2, class T3, class T4>
-struct RightTrimTuple<T1, T2, T3, T4, NoElement, NoElement, NoElement, NoElement>
-{
-  typedef Dune::tuple<T1, T2, T3, T4> type;
-};
-
-template <class T1, class T2, class T3, class T4, class T5>
-struct RightTrimTuple<T1, T2, T3, T4, T5, NoElement, NoElement, NoElement>
-{
-  typedef Dune::tuple<T1, T2, T3, T4, T5> type;
-};
-
-template <class T1, class T2, class T3, class T4, class T5, class T6>
-struct RightTrimTuple<T1, T2, T3, T4, T5, T6, NoElement, NoElement>
-{
-  typedef Dune::tuple<T1, T2, T3, T4, T5, T6> type;
-};
-
-template <class T1, class T2, class T3, class T4, class T5, class T6, class T7>
-struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, NoElement>
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 7>
 {
   typedef Dune::tuple<T1, T2, T3, T4, T5, T6, T7> type;
 };
 
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 6>
+{
+  typedef Dune::tuple<T1, T2, T3, T4, T5, T6> type;
+};
+
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 5>
+{
+  typedef Dune::tuple<T1, T2, T3, T4, T5> type;
+};
+
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 4>
+{
+  typedef Dune::tuple<T1, T2, T3, T4> type;
+};
+
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 3>
+{
+  typedef Dune::tuple<T1, T2, T3> type;
+};
+
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 2>
+{
+  typedef Dune::tuple<T1, T2> type;
+};
+
+template <class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9>
+struct RightTrimTuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, 1>
+{
+  typedef Dune::tuple<T1> type;
+};
+
 #define TUPLE_TYPEDEFS_2_TUPLE(t_, s_)                                                                                 \
-  typename RightTrimTuple<TELE(t_, s_, 1),                                                                             \
+  typename RightTrimTuple<TELE(t_, s_, 0),                                                                             \
+                          TELE(t_, s_, 1),                                                                             \
                           TELE(t_, s_, 2),                                                                             \
                           TELE(t_, s_, 3),                                                                             \
                           TELE(t_, s_, 4),                                                                             \
                           TELE(t_, s_, 5),                                                                             \
                           TELE(t_, s_, 6),                                                                             \
                           TELE(t_, s_, 7),                                                                             \
-                          TEL(t_, s_, 8)>::type
+                          TELE(t_, s_, 8),                                                                             \
+                          Dune::tuple_size<t_>::value>::type
+
 
 /**
  * @def TUPLE_TYPEDEFS_2_TUPLE( t_, s_ )
