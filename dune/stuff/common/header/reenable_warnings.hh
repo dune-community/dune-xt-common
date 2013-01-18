5efe5176b70ab37a9b0288adeae3a560344744d4
Felix Albrecht <felix.albrecht@uni-muenster.de>
2013-01-18 16:42:03 +0100

    [common.header] fixed

--- dune/stuff/common/header/reenable_warnings.hh
+++ dune/stuff/common/header/reenable_warnings.hh
@@ -1,11 +1,4 @@
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wignored-qualifiers"
-#pragma GCC diagnostic ignored "-Wextra"
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#pragma GCC diagnostic ignored "-Wtype-limits"
-#pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic pop
 
 /** Copyright (c) 2012, Rene Milk
    * All rights reserved.
