fd06536f04164a0b868015b237732beeba0aae2e
Rene Milk <rene.milk@uni-muenster.de>
2013-02-13 17:16:27 +0100

    [tests] limit logging to error

--- dune/stuff/test/common_tuple.cc
+++ dune/stuff/test/common_tuple.cc
@@ -47,5 +47,6 @@ int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
   Dune::MPIHelper::instance(argc, argv);
+  DSC::Logger().create(DSC::LOG_CONSOLE | DSC::LOG_ERROR);
   return RUN_ALL_TESTS();
 }
