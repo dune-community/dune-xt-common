c975bca659e1334282717796caca03577dd94408
Felix Albrecht <mail@felixalbrecht.de>
2013-03-12 18:45:02 +0100

    [common.string] removed equal()

--- dune/stuff/common/string.hh
+++ dune/stuff/common/string.hh
@@ -149,12 +149,6 @@ std::string to(const T& t)
   return s;
 }
 
-DUNE_DEPRECATED_MSG("nonsense function, to be removed immenently")
-bool equal(const std::string& first, const std::string& second)
-{
-  return !first.compare(second);
-}
-
 DUNE_DEPRECATED_MSG("use the constructor call directly")
 std::vector<std::string> mainArgsToVector(int argc, char** argv)
 {
