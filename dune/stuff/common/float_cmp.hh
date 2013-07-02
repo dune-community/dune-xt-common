6cbf2f77810e1cdb81086ba5f2bf241a5f264f07
Felix Albrecht <mail@felixalbrecht.de>
2013-07-03 01:17:37 +0200

    [common.float_cmp] added EigenDenseVector to eq()

--- dune/stuff/common/float_cmp.hh
+++ dune/stuff/common/float_cmp.hh
@@ -4,10 +4,15 @@
 #include <dune/common/float_cmp.hh>
 #include <dune/common/dynvector.hh>
 
+#if HAVE_EIGEN
+#include <dune/stuff/la/container/eigen.hh>
+#endif
+
 namespace Dune {
 namespace Stuff {
 namespace Common {
 namespace FloatCmp {
+
 
 template <class T, Dune::FloatCmp::CmpStyle style = Dune::FloatCmp::defaultCmpStyle>
 bool eq(const Dune::DynamicVector<T>& first, const Dune::DynamicVector<T>& second,
@@ -20,6 +25,32 @@ bool eq(const Dune::DynamicVector<T>& first, const Dune::DynamicVector<T>& secon
       result = false;
   return result;
 } // ... eq(...)
+
+#if HAVE_EIGEN
+template <class T, Dune::FloatCmp::CmpStyle style = Dune::FloatCmp::defaultCmpStyle>
+bool eq(const Dune::Stuff::LA::EigenDenseVector<T>& first, const Dune::Stuff::LA::EigenDenseVector<T>& second,
+        typename Dune::FloatCmp::EpsilonType<T>::Type epsilon = Dune::FloatCmp::DefaultEpsilon<T, style>::value())
+{
+  assert(first.size() == second.size());
+  bool result = true;
+  for (size_t ii = 0; ii < first.size(); ++ii)
+    if (!Dune::FloatCmp::eq<T, style>(first.backend()[ii], second.backend()[ii], epsilon))
+      result = false;
+  return result;
+} // ... eq(...)
+
+template <class T, Dune::FloatCmp::CmpStyle style = Dune::FloatCmp::defaultCmpStyle>
+bool eq(const Dune::DynamicVector<T>& first, const Dune::Stuff::LA::EigenDenseVector<T>& second,
+        typename Dune::FloatCmp::EpsilonType<T>::Type epsilon = Dune::FloatCmp::DefaultEpsilon<T, style>::value())
+{
+  assert(first.size() == second.size());
+  bool result = true;
+  for (size_t ii = 0; ii < first.size(); ++ii)
+    if (!Dune::FloatCmp::eq<T, style>(first[ii], second.backend()[ii], epsilon))
+      result = false;
+  return result;
+} // ... eq(...)
+#endif // HAVE_EIGEN
 
 } // namespace FloatCmp
 } // namespace Common
