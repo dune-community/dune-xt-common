e18ca14279f7bb3d32325447c7e6584b9f6db478
Felix Albrecht <mail@felixalbrecht.de>
2013-07-08 23:31:53 +0200

    [common.float_cmp] added eq() for Dune::FieldVector

--- dune/stuff/common/float_cmp.hh
+++ dune/stuff/common/float_cmp.hh
@@ -3,6 +3,7 @@
 
 #include <dune/common/float_cmp.hh>
 #include <dune/common/dynvector.hh>
+#include <dune/common/fvector.hh>
 
 #if HAVE_EIGEN
 #include <dune/stuff/la/container/eigen.hh>
@@ -21,6 +22,17 @@ bool eq(const Dune::DynamicVector<T>& first, const Dune::DynamicVector<T>& secon
   assert(first.size() == second.size());
   bool result = true;
   for (size_t ii = 0; ii < first.size(); ++ii)
+    if (!Dune::FloatCmp::eq<T, style>(first[ii], second[ii], epsilon))
+      result = false;
+  return result;
+} // ... eq(...)
+
+template <class T, int size, Dune::FloatCmp::CmpStyle style = Dune::FloatCmp::defaultCmpStyle>
+bool eq(const Dune::FieldVector<T, size>& first, const Dune::FieldVector<T, size>& second,
+        typename Dune::FloatCmp::EpsilonType<T>::Type epsilon = Dune::FloatCmp::DefaultEpsilon<T, style>::value())
+{
+  bool result = true;
+  for (size_t ii = 0; ii < size; ++ii)
     if (!Dune::FloatCmp::eq<T, style>(first[ii], second[ii], epsilon))
       result = false;
   return result;
