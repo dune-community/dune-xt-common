c371fb51549623e4a5f32cbef419a723d652edb9
Rene Milk <rene.milk@uni-muenster.de>
2013-07-19 00:45:03 +0200

    stuff is now buildable with dune-cmake

--- dune/stuff/common/math.cc
+++ dune/stuff/common/math.cc
@@ -1,4 +1,4 @@
 #include "math.hh"
 
 // this is just for the test to compile
-const std::string Epsilon<std::string, false>::value = "_";
+const std::string Dune::Stuff::Common::Epsilon<std::string, false>::value = "_";
