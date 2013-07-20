b8e57a6ae12e4e0fdfd1b47fcb0884baadc1f5ac
Felix Albrecht <mail@felixalbrecht.de>
2013-07-20 18:06:31 +0200

    [lib] added math

--- dune/stuff/common/math.cc
+++ dune/stuff/common/math.cc
@@ -1,4 +1,12 @@
 #include "math.hh"
 
+namespace Dune {
+namespace Stuff {
+namespace Common {
+
 // this is just for the test to compile
 const std::string Epsilon<std::string, false>::value = "_";
+
+} // namespace Common
+} // namespace Stuff
+} // namespace Dune
