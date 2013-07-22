ac0acaccece4f69c8bdd1df8922d74cdce55b559
Rene Milk <rene.milk@uni-muenster.de>
2013-07-22 12:02:37 +0200

    [common] fix the damn StraightenBlockVector call forever

--- dune/stuff/common/math.hh
+++ dune/stuff/common/math.hh
@@ -35,9 +35,14 @@
 
 namespace boost {
 namespace math {
+
+// use the limited scope here to no longer care about which effing fem version we compile against
+using namespace Dune;
+using namespace Dune::Fem;
+
 //! isinf specialization for Dune::StraightenBlockVector
 template <class BlockVectorImp, class DofImp>
-inline bool isinf(const Dune::Fem::StraightenBlockVector<BlockVectorImp, DofImp>& x)
+inline bool isinf(const StraightenBlockVector<BlockVectorImp, DofImp>& x)
 {
   for (size_t i = 0; i < x.size(); ++i) {
     if (std::isinf(x[i]))
