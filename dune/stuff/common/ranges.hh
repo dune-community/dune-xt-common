9b5254ee5b2a52948490b413a208315b9fc76dd1
Felix Albrecht <mail@felixalbrecht.de>
2013-09-27 22:45:36 +0200

    [common.ranges] added fem guard

--- dune/stuff/common/ranges.hh
+++ dune/stuff/common/ranges.hh
@@ -17,11 +17,16 @@
 
 #if HAVE_DUNE_FEM
 #include <dune/fem/version.hh>
+#include <dune/stuff/fem/namespace.hh>
 #include <dune/stuff/common/disable_warnings.hh>
 #include <dune/fem/function/common/discretefunction.hh>
 #include <dune/stuff/common/reenable_warnings.hh>
 #include <dune/fem/gridpart/common/gridpart.hh>
+#if DUNE_FEM_IS_LOCALFUNCTIONS_COMPATIBLE
+#include <dune/fem/space/lagrangespace/lagrangepoints.hh>
+#else
 #include <dune/fem/space/lagrange/lagrangepoints.hh>
+#endif
 #endif
 #include <dune/stuff/common/math.hh>
 #include <dune/stuff/fem/namespace.hh>
