7084590a0642d493bf7e30bbe6b3596ff4e84bd6
Felix Albrecht <mail@felixalbrecht.de>
2013-10-17 00:51:12 +0200

    [cmake] major change in GridUtils * we add the include dirs and link dirs per grid type   depending only on the fact, that the grid was found * we also add the libs of all found grids to GRIDLIBS * depending on the users choice of GRIDTYPE we only   set stuff like -DENABLE_FOO for the   Dune::GridSelector, nothing else All in all, this should finally allow to use different grids in one place (e.g. lib).

--- cmake/GridUtils.cmake
+++ cmake/GridUtils.cmake
@@ -12,32 +12,38 @@ SET( GRIDDIM
 add_definitions( "-D${GRIDTYPE}" )
 add_definitions( "-DGRIDDIM=${GRIDDIM}" )
 
+if(UG_GRID_FOUND)
+  include_sys_dir( ${UG_GRID_INCLUDE_DIRS} )
+  link_directories( ${UG_GRID_LIBRARY_DIRS} )
+  # althoug we use pkg_config to find ug we do not use UG_LIBRARIES,
+  # because those listed in lubug.pc are wrong
+  set(UGGRID_LIBS ugS2 ugS3 devS m)
+endif(UG_GRID_FOUND)
+
 set( ENABLE_UG OFF )
 if( ${GRIDTYPE} MATCHES "UGGRID" )
   if( NOT UG_GRID_FOUND )
     message( FATAL_ERROR "Gridtype ${GRIDTYPE} requested, but libug was not found!" )
   endif( NOT UG_GRID_FOUND )
-  include_sys_dir( ${UG_GRID_INCLUDE_DIRS} )
-  link_directories( ${UG_GRID_LIBRARY_DIRS} )
-  # althoug we use pkg_config to find ug we do not use UG_LIBRARIES,
-  # because those listed in lubug.pc are wrong
-  set( GRIDLIBS ugS2 ugS3 devS m )
   add_definitions( "-DENABLE_UG=1" )
 endif( ${GRIDTYPE} MATCHES "UGGRID" )
+
+if(ALU_GRID_FOUND)
+  include_sys_dir( ${ALU_GRID_INCLUDE_DIRS} )
+  include_sys_dir( ${ALU_GRID_INCLUDE_DIRS}/serial )
+  include_sys_dir( ${ALU_GRID_INCLUDE_DIRS}/duneinterface )
+	if(ENABLE_MPI)
+		include_sys_dir( ${ALUGRID_BASE_PATH}/include/parallel )
+	endif(ENABLE_MPI)
+  link_directories( ${ALU_GRID_LIBRARY_DIRS} )
+  set(ALUGRID_LIBS ${ALU_GRID_LIBRARIES} )
+endif(ALU_GRID_FOUND)
 
 SET( ENABLE_ALUGRID OFF )
 if( ${GRIDTYPE} MATCHES "ALUGRID" )
   if( NOT ALU_GRID_FOUND )
     message( FATAL_ERROR "Gridtype ${GRIDTYPE} requested, but alugrid was not found!" )
   endif( NOT ALU_GRID_FOUND )
-  include_sys_dir( ${ALU_GRID_INCLUDE_DIRS} )
-  include_sys_dir( ${ALU_GRID_INCLUDE_DIRS}/serial )
-  include_sys_dir( ${ALU_GRID_INCLUDE_DIRS}/duneinterface )
-  link_directories( ${ALU_GRID_LIBRARY_DIRS} )
-  set( GRIDLIBS ${ALU_GRID_LIBRARIES} )
-	if( ENABLE_MPI )
-		include_sys_dir( ${ALUGRID_BASE_PATH}/include/parallel )
-	endif( ENABLE_MPI )
 	add_definitions( "-DENABLE_ALUGRID=1" )
 endif( ${GRIDTYPE} MATCHES "ALUGRID" )
 
@@ -50,3 +56,5 @@ endif( ${GRIDTYPE} MATCHES "ALUGRID" )
 	#SET( ENABLE_ALBERTA ON )
 	#ADD_DEFINITIONS( -DENABLE_ALBERTA=1 -DDEBUG )
 #ENDIF( ${GRIDTYPE} MATCHES "ALBERTAGRID" )
+
+set( GRIDLIBS ${UGGRID_LIBS} ${ALUGRID_LIBS} )
