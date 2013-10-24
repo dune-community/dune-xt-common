dafadc45d34220e5b27216ce5f80a35201ae969e
Felix Albrecht <mail@felixalbrecht.de>
2013-10-24 15:16:50 +0200

    [common.header] removed * fixes #252

--- dune/stuff/common/header/disable_warnings.hh
+++
@@ -1,2 +0,0 @@
-#warning "This header is deprecated, use #include <dune/stuff/common/disable_warnings.hh> instead!"
-#include <dune/stuff/common/disable_warnings.hh>
--- dune/stuff/common/header/reenable_warnings.hh
+++
@@ -1,2 +0,0 @@
-#warning "This header is deprecated, use #include <dune/stuff/common/reenable_warnings.hh> instead!"
-#include <dune/stuff/common/reenable_warnings.hh>
