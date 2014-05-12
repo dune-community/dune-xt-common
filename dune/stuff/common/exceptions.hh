0ba69d2947eef7c2ef8eef680e50942e521917b7
Felix Schindler <felix@schindlerfamily.de>
2014-05-12 11:35:29 +0200

    [common.exceptions] have less top categories

--- dune/stuff/common/exceptions.hh
+++ dune/stuff/common/exceptions.hh
@@ -61,11 +61,15 @@ class index_out_of_range : public Dune::Exception
 {
 };
 
-class wrong_input_given : public Dune::Exception
+class you_are_using_this_wrongly : public Dune::Exception
 {
 };
 
-class you_are_using_this_wrongly : public Dune::Exception
+class wrong_input_given : public you_are_using_this_wrongly
+{
+};
+
+class requirements_not_met : public you_are_using_this_wrongly
 {
 };
 
