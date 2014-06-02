505cba3ee9f4794a856e6292cb2e53f05f7005d5
Felix Schindler <felix@schindlerfamily.de>
2014-06-02 22:50:41 +0200

    [common.fvector] added a FieldVector variant with sane ctors

---
+++ dune/stuff/common/fvector.hh
@@ -0,0 +1,90 @@
+// This file is part of the dune-stuff project:
+//   https://users.dune-project.org/projects/dune-stuff
+// Copyright holders: Rene Milk, Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#ifndef DUNE_STUFF_COMMON_FVECTOR_HH
+#define DUNE_STUFF_COMMON_FVECTOR_HH
+
+#include <vector>
+#include <initializer_list>
+
+#include <dune/common/fvector.hh>
+
+#include <dune/stuff/common/exceptions.hh>
+
+namespace Dune {
+namespace Stuff {
+namespace Common {
+
+
+template <class K, int SIZE>
+class FieldVector : public Dune::FieldVector<K, SIZE>
+{
+  typedef Dune::FieldVector<K, SIZE> BaseType;
+  typedef FieldVector<K, SIZE> ThisType;
+
+public:
+  FieldVector(const size_t sz, const K kk = K(0))
+    : BaseType(kk)
+  {
+#ifndef NDEBUG
+    if (sz != SIZE)
+      DUNE_THROW_COLORFULLY(Exceptions::wrong_input_given,
+                            "You are trying to construct a FieldVector< ..., " << SIZE << " > (of "
+                                                                               << "static size) with a size of "
+                                                                               << sz
+                                                                               << "!");
+#endif // NDEBUG
+  } // ... FieldVector(...)
+
+  FieldVector(const BaseType& other)
+    : BaseType(other)
+  {
+  }
+
+  FieldVector(const std::vector<K>& vec)
+    : BaseType(K(0))
+  {
+#ifndef NDEBUG
+    if (vec.size() != SIZE)
+      DUNE_THROW_COLORFULLY(Exceptions::wrong_input_given,
+                            "You are trying to construct a FieldVector< ..., " << SIZE << " > (of "
+                                                                               << "static size) from a vector of size "
+                                                                               << vec.size()
+                                                                               << "!");
+#endif // NDEBUG
+    for (size_t ii = 0; ii < SIZE; ++ii)
+      this->operator[](ii) = vec[ii];
+  } // FieldVector(...)
+
+  FieldVector(std::initializer_list<K> list)
+    : BaseType(K(0))
+  {
+#ifndef NDEBUG
+    if (list.size() != SIZE)
+      DUNE_THROW_COLORFULLY(Exceptions::wrong_input_given,
+                            "You are trying to construct a FieldVector< ..., " << SIZE << " > (of "
+                                                                               << "static size) from a list of size "
+                                                                               << list.size()
+                                                                               << "!");
+#endif // NDEBUG
+    size_t ii = 0;
+    for (auto element : list) {
+      this->operator[](ii) = element;
+      ++ii;
+    }
+  } // FieldVector(...)
+
+  ThisType& operator=(const BaseType& other)
+  {
+    return BaseType::operator=(other);
+  }
+}; // class FieldVector
+
+
+} // namespace Common
+} // namespace Stuff
+} // namespace Dune
+
+#endif // DUNE_STUFF_COMMON_FVECTOR_HH
