3da7bd46e639ee447ddd6ee1939d0d3b39d51ef0
Felix Schindler <felix.schindler@wwu.de>
2014-08-12 16:23:09 +0200

    [common.string]
    * moved Choose* to internal namespace
    * reenabled printing of typename
    * some minor updated

--- dune/stuff/common/string.hh
+++ dune/stuff/common/string.hh
@@ -42,13 +42,10 @@
 #include <dune/stuff/common/fvector.hh>
 #include <dune/stuff/common/fmatrix.hh>
 #include <dune/stuff/common/debug.hh>
+#include <dune/stuff/la/container/interfaces.hh>
 #include <dune/stuff/la/container/common.hh>
-#if HAVE_EIGEN
 #include <dune/stuff/la/container/eigen.hh>
-#endif
-#if HAVE_DUNE_ISTL
 #include <dune/stuff/la/container/istl.hh>
-#endif
 
 namespace Dune {
 namespace Stuff {
@@ -61,6 +58,10 @@ template <class T = std::string>
 inline std::vector<T>
 tokenize(const std::string& msg, const std::string& separators,
          const boost::algorithm::token_compress_mode_type mode = boost::algorithm::token_compress_off);
+
+
+namespace internal {
+
 
 /* forward to use Choose< S >::fromString in convert_from_string_safely method below. As Choose< S >::fromString does
 not depend on convert_from_string_safely for scalar types, there should be no endless recursion */
@@ -121,6 +122,7 @@ class ChooseBase
     }
   };
 #endif // HAVE_EIGEN
+
 public:
   template <class M>
   struct MatrixGetter
@@ -153,6 +155,7 @@ public:
     }
   };
 #endif // HAVE_EIGEN
+
 private:
   static std::string trim_copy_safely(const std::string& str_in)
   {
@@ -285,7 +288,8 @@ public:
   static inline ReturnType fromString(const std::string s, const size_t UNUSED_UNLESS_DEBUG(rows) = 0,
                                       const size_t UNUSED_UNLESS_DEBUG(cols) = 0)
   {
-    assert(rows == 0 && cols == 0);
+    assert(rows == 0);
+    assert(cols == 0);
     return boost::lexical_cast<ReturnType, std::string>(s);
   }
 }; // ... Choose < ReturnType >
@@ -301,7 +305,8 @@ public:
   static inline char fromString(const std::string s, const size_t UNUSED_UNLESS_DEBUG(rows) = 0,
                                 const size_t UNUSED_UNLESS_DEBUG(cols) = 0)
   {
-    assert(rows == 0 && cols == 0);
+    assert(rows == 0);
+    assert(cols == 0);
     if (s.size() == 1)
       return s[0];
     else {
@@ -321,7 +326,8 @@ public:
   static inline const char* fromString(const std::string s, const size_t UNUSED_UNLESS_DEBUG(rows) = 0,
                                        const size_t UNUSED_UNLESS_DEBUG(cols) = 0)
   {
-    assert(rows == 0 && cols == 0);
+    assert(rows == 0);
+    assert(cols == 0);
     return s.c_str();
   } // ... fromString(...)
 }; // class Choose < const char * >
@@ -335,7 +341,8 @@ public:
     static inline tn fromString(const std::string s, const size_t UNUSED_UNLESS_DEBUG(rows) = 0,                       \
                                 const size_t UNUSED_UNLESS_DEBUG(cols) = 0)                                            \
     {                                                                                                                  \
-      assert(rows == 0 && cols == 0);                                                                                  \
+      assert(rows == 0);                                                                                               \
+      assert(cols == 0);                                                                                               \
       return std::sto##tns(s);                                                                                         \
     }                                                                                                                  \
   };
@@ -385,16 +392,15 @@ DSC_MATRIXFRSTR(LA::EigenDenseMatrix)
                                                             const size_t cols = 0)                                     \
     {                                                                                                                  \
       if ((rows > 0 && rows != ROWS) || (cols > 0 && cols != COLS))                                                    \
-        DUNE_THROW_COLORFULLY(Exceptions::configuration_error,                                                         \
-                              "You requested a '" /*<< Typename< FieldMatrixType >::value() <<*/ "' with a size of "   \
-                                  << rows                                                                              \
-                                  << "x"                                                                               \
-                                  << cols                                                                              \
-                                  << " but this type of matrix can not have any size other than "                      \
-                                  << ROWS                                                                              \
-                                  << "x"                                                                               \
-                                  << COLS                                                                              \
-                                  << "!");                                                                             \
+        DUNE_THROW_COLORFULLY(                                                                                         \
+            Exceptions::configuration_error,                                                                           \
+            "You requested a '" << getTypename(FieldMatrixType<S, ROWS, COLS>()) << "' with a size of " << rows << "x" \
+                                << cols                                                                                \
+                                << " but this type of matrix can not have any size other than "                        \
+                                << ROWS                                                                                \
+                                << "x"                                                                                 \
+                                << COLS                                                                                \
+                                << "!");                                                                               \
       return BaseType::template get_matrix_from_string<FieldMatrixType<S, ROWS, COLS>, S>(s, ROWS, COLS);              \
     }                                                                                                                  \
   };
@@ -462,6 +468,9 @@ DSC_FIELDVECTORFRSTR(Dune::Stuff::Common::FieldVector)
 #undef DSC_FIELDVECTORFRSTR
 
 
+} // namespace internal
+
+
 /** \brief Read an object of type ReturnType from a string
  *  \param s string to read from
  *  \param size Determines the size of the returning container (size if ReturnType is a vector type, rows if ReturnType
@@ -472,7 +481,7 @@ DSC_FIELDVECTORFRSTR(Dune::Stuff::Common::FieldVector)
 template <class ReturnType>
 ReturnType fromString(const std::string s, const size_t size = 0, const size_t cols = 0)
 {
-  return Choose<ReturnType>::fromString(s, size, cols);
+  return internal::Choose<ReturnType>::fromString(s, size, cols);
 } // class fromString(...)
 
 
@@ -563,8 +572,9 @@ DSC_FIELDVECTORTOSTR(Dune::FieldVector)
       for (size_t cc = 0; cc < matrix.cols(); ++cc) {                                                                  \
         if (cc > 0)                                                                                                    \
           str += " ";                                                                                                  \
-        str += toString(                                                                                               \
-            ChooseBase<MatrixType<S>>::template MatrixGetter<MatrixType<S>>::template get_entry<S>(matrix, rr, cc));   \
+        str +=                                                                                                         \
+            toString(internal::ChooseBase<MatrixType<S>>::template MatrixGetter<MatrixType<S>>::template get_entry<S>( \
+                matrix, rr, cc));                                                                                      \
       }                                                                                                                \
     }                                                                                                                  \
     str += "]";                                                                                                        \
@@ -592,8 +602,8 @@ DSC_MATRIXTOSTR(LA::EigenDenseMatrix)
         if (cc > 0)                                                                                                    \
           str += " ";                                                                                                  \
         str += toString(                                                                                               \
-            ChooseBase<FieldMatrixType<S, ROWS, COLS>>::template MatrixGetter<FieldMatrixType<S, ROWS, COLS>>::        \
-                template get_entry<S>(matrix, rr, cc));                                                                \
+            internal::ChooseBase<FieldMatrixType<S, ROWS, COLS>>::                                                     \
+                template MatrixGetter<FieldMatrixType<S, ROWS, COLS>>::template get_entry<S>(matrix, rr, cc));         \
       }                                                                                                                \
     }                                                                                                                  \
     str += "]";                                                                                                        \
