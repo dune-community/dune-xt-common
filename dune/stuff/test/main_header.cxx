e11d5e13af53a96c7087a12c703e3ff7a17daee3
Felix Schindler <felix.schindler@wwu.de>
2014-08-28 11:30:57 +0200

    [test.main_header] added
    This one should me renamed to main.hh once the deprecation grace period
    for the old main.hh is over. The .cxx suffix is there to avoid automatic
    test generation for this file.

---
+++ dune/stuff/test/main_header.cxx
@@ -0,0 +1,50 @@
+// This file is part of the dune-stuff project:
+//   https://github.com/wwu-numerik/dune-stuff
+// Copyright holders: Rene Milk, Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+#include "config.h"
+
+#include <sys/time.h>
+
+#include <dune/stuff/common/disable_warnings.hh>
+#include <dune/stuff/test/gtest/gtest.h>
+#include <dune/stuff/common/reenable_warnings.hh>
+
+#include "main_header.hh"
+
+void busywait(const int ms)
+{
+  // "round" up to next full 10 ms to align with native timer res
+  const int milliseconds = (ms / 10) * 10 + 10;
+  timeval start, end;
+  gettimeofday(&start, NULL);
+  do {
+    gettimeofday(&end, NULL);
+  } while (((end.tv_sec - start.tv_sec) * 1e6) + ((end.tv_usec - start.tv_usec)) < milliseconds * 1000);
+} // ... busywait(...)
+
+
+namespace Dune {
+namespace Stuff {
+namespace Test {
+
+
+void check_for_success(const Dune::Stuff::Common::ConvergenceStudy& study,
+                       const std::map<std::string, std::vector<double>>& errors_map)
+{
+  for (const auto& norm : study.used_norms()) {
+    const auto expected_results = study.expected_results(norm);
+    const auto errors_search = errors_map.find(norm);
+    EXPECT_NE(errors_search, errors_map.end()) << "          norm = " << norm;
+    const auto& errors = errors_search->second;
+    EXPECT_LE(errors.size(), expected_results.size()) << "          norm = " << norm;
+    for (size_t ii = 0; ii < errors.size(); ++ii)
+      EXPECT_LE(errors[ii], expected_results[ii]) << "          norm = " << norm << ", level = " << ii;
+  }
+} // ... check_for_success(...)
+
+
+} // namespace Test
+} // namespace Stuff
+} // namespace Dune
---
+++ dune/stuff/test/main_header.hh
@@ -0,0 +1,77 @@
+// This file is part of the dune-stuff project:
+//   https://github.com/wwu-numerik/dune-stuff
+// Copyright holders: Rene Milk, Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+/**
+  * After a grace period this header should be moved to main.hh.
+  **/
+
+#ifndef DUNE_STUFF_TEST_MAIN_HEADER_HH
+#define DUNE_STUFF_TEST_MAIN_HEADER_HH
+
+#include <string>
+#include <map>
+#include <vector>
+
+#include <dune/stuff/common/disable_warnings.hh>
+#include <dune/common/tuples.hh>
+#include <dune/common/tupleutility.hh>
+#include <dune/stuff/common/reenable_warnings.hh>
+
+#include <dune/stuff/common/convergence-study.hh>
+
+template <template <class> class Test>
+struct TestRunner
+{
+  struct Visitor
+  {
+    template <class T>
+    void visit(const T&)
+    {
+      Test<T>().run();
+    }
+  };
+
+  template <class Tuple>
+  static void run()
+  {
+    Tuple t;
+    Dune::ForEachValue<Tuple> fe(t);
+    Visitor v;
+    fe.apply(v);
+  }
+}; // struct TestRunner
+
+
+template <int i>
+struct Int
+{
+  static const int value = i;
+};
+
+
+typedef Dune::tuple<double, float
+                    //                   , Dune::bigunsignedint
+                    ,
+                    int, unsigned int, unsigned long, long long, char> BasicTypes;
+
+
+//! where sleep only counts toward wall time, this wastes actual cpu time
+void busywait(const int ms);
+
+
+namespace Dune {
+namespace Stuff {
+namespace Test {
+
+
+void check_for_success(const Dune::Stuff::Common::ConvergenceStudy& study,
+                       const std::map<std::string, std::vector<double>>& errors_map);
+
+
+} // namespace Test
+} // namespace Stuff
+} // namespace Dune
+
+#endif // DUNE_STUFF_TEST_MAIN_HEADER_HH
