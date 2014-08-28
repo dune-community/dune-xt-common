b21aa385b98b43d77956d10ff5144c49a5745fc4
Felix Schindler <felix.schindler@wwu.de>
2014-08-28 11:31:24 +0200

    [test.main] use main_header

--- dune/stuff/test/main.hxx
+++ dune/stuff/test/main.hxx
@@ -13,7 +13,6 @@
 #include <map>
 #include <random>
 #include <fstream>
-#include <sys/time.h>
 
 #include <dune/stuff/common/disable_warnings.hh>
 #include <dune/stuff/test/gtest/gtest.h>
@@ -21,8 +20,6 @@
 #include <dune/common/float_cmp.hh>
 #include <dune/common/fvector.hh>
 #include <dune/common/fmatrix.hh>
-#include <dune/common/tuples.hh>
-#include <dune/common/tupleutility.hh>
 #include <dune/common/parallel/mpihelper.hh>
 
 #if HAVE_DUNE_FEM
@@ -35,20 +32,7 @@
 #include <dune/stuff/common/logging.hh>
 #include <dune/stuff/common/convergence-study.hh>
 
-
-static void check_for_success(const Dune::Stuff::Common::ConvergenceStudy& study,
-                              const std::map<std::string, std::vector<double>>& errors_map)
-{
-  for (const auto& norm : study.used_norms()) {
-    const auto expected_results = study.expected_results(norm);
-    const auto errors_search = errors_map.find(norm);
-    EXPECT_NE(errors_search, errors_map.end()) << "          norm = " << norm;
-    const auto& errors = errors_search->second;
-    EXPECT_LE(errors.size(), expected_results.size()) << "          norm = " << norm;
-    for (size_t ii = 0; ii < errors.size(); ++ii)
-      EXPECT_LE(errors[ii], expected_results[ii]) << "          norm = " << norm << ", level = " << ii;
-  }
-} // ... check_for_success(...)
+#include "main_header.hh"
 
 
 class DUNE_DEPRECATED_MSG("Use the expectation macros of the gtest test suite (20.08.2014)!") errors_are_not_as_expected
@@ -70,55 +54,6 @@ std::vector<double> DUNE_DEPRECATED_MSG("Use the expectation macros of the gtest
     return ret;
   }
 } // ... truncate_vector(...)
-
-
-template <template <class> class Test>
-struct TestRunner
-{
-  struct Visitor
-  {
-    template <class T>
-    void visit(const T&)
-    {
-      Test<T>().run();
-    }
-  };
-
-  template <class Tuple>
-  static void run()
-  {
-    Tuple t;
-    Dune::ForEachValue<Tuple> fe(t);
-    Visitor v;
-    fe.apply(v);
-  }
-}; // struct TestRunner
-
-
-template <int i>
-struct Int
-{
-  static const int value = i;
-};
-
-
-//! where sleep only counts toward wall time, this wastes actual cpu time
-void busywait(const int ms)
-{
-  // "round" up to next full 10 ms to align with native timer res
-  const int milliseconds = (ms / 10) * 10 + 10;
-  timeval start, end;
-  gettimeofday(&start, NULL);
-  do {
-    gettimeofday(&end, NULL);
-  } while (((end.tv_sec - start.tv_sec) * 1e6) + ((end.tv_usec - start.tv_usec)) < milliseconds * 1000);
-}
-
-
-typedef Dune::tuple<double, float
-                    //                   , Dune::bigunsignedint
-                    ,
-                    int, unsigned int, unsigned long, long long, char> BasicTypes;
 
 
 int main(int argc, char** argv)
