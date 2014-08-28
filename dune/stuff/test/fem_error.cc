98089f8a0e16dcbe7a8bf0866822a05c2d3eb23f
Felix Schindler <felix.schindler@wwu.de>
2014-08-28 12:21:47 +0200

    [test] removed empty fem_error

--- dune/stuff/test/fem_error.cc
+++
@@ -1,8 +0,0 @@
-// This file is part of the dune-stuff project:
-//   https://github.com/wwu-numerik/dune-stuff
-// Copyright holders: Rene Milk, Felix Schindler
-// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-
-#include "test_common.hh"
-
-#include <dune/stuff/test/test_main.cxx>
