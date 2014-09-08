306b25ea7af7fd63a9075f1d205d5ddb6d5f824d
Felix Schindler <felix.schindler@wwu.de>
2014-09-08 16:00:32 +0200

    [test.common_string] added another boolean testcase

--- dune/stuff/test/common_string.cc
+++ dune/stuff/test/common_string.cc
@@ -156,8 +156,10 @@ TEST(StringTest, ConvertFrom)
   EXPECT_THROW(fromString<char>("sd"), Dune::Stuff::Exceptions::wrong_input_given);
   EXPECT_EQ(true, fromString<bool>("1"));
   EXPECT_EQ(true, fromString<bool>("true"));
+  EXPECT_EQ(true, fromString<bool>("True"));
   EXPECT_EQ(false, fromString<bool>("0"));
   EXPECT_EQ(false, fromString<bool>("false"));
+  EXPECT_EQ(false, fromString<bool>("False"));
   EXPECT_THROW(fromString<int>(""), std::invalid_argument);
 }
 
