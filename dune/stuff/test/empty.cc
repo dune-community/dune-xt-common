d1ee2e76257bbdcdacd5efa8e920063078e8e3f0
Felix Schindler <felix.schindler@wwu.de>
2014-09-11 20:51:18 +0200

    [test.empty] added

---
+++ dune/stuff/test/empty.cc
@@ -0,0 +1,14 @@
+// This file is part of the dune-stuff project:
+//   https://github.com/wwu-numerik/dune-stuff
+// Copyright holders: Rene Milk, Felix Schindler
+// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+
+/**
+  * This file is intended as a starting point for quick testing.
+  */
+
+#include "main.hxx"
+
+TEST(empty, main)
+{
+}
