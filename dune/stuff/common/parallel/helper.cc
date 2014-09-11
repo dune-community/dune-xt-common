a992a5666cdc2f9f2f8f618676b10b8ce7000046
Rene Milk <rene.milk@uni-muenster.de>
2014-09-11 18:03:37 +0200

    [common.parallelhelper] don't try to abort solitary mpi procs

--- dune/stuff/common/parallel/helper.cc
+++ dune/stuff/common/parallel/helper.cc
@@ -11,7 +11,9 @@
 int Dune::Stuff::abort_all_mpi_processes()
 {
 #if HAVE_MPI
-  return MPI_Abort(MPIHelper::getCommunicator(), 1);
+  if (MPIHelper::getCollectiveCommunication().size() > 1)
+    return MPI_Abort(MPIHelper::getCommunicator(), 1);
+  else
 #endif
-  return 1;
+    return 1;
 }
