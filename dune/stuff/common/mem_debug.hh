297e2d49f6474f2de31d1f60fee0f377574fa977
Rene Milk <rene.milk@uni-muenster.de>
2014-09-25 13:24:57 +0200

    removes obsolete mem_debug header

--- dune/stuff/common/mem_debug.hh
+++
@@ -1,6 +0,0 @@
-// This file is part of the dune-stuff project:
-//   https://github.com/wwu-numerik/dune-stuff
-// Copyright holders: Rene Milk, Felix Schindler
-// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-
-#warning "use something sane, like http://duma.sourceforge.net/ or valgrind"
