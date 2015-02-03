c02299fa813e84469cdd9a74abeb5775702d8212
Felix Schindler <felix.schindler@wwu.de>
2015-02-03 15:17:27 +0100

    [test.common_color] refs #10

--- dune/stuff/test/common_color.cc
+++ dune/stuff/test/common_color.cc
@@ -41,7 +41,7 @@ TEST(Color, All)
   std::cout << StreamModifiers::underline
             << "colored 'hello world' for all available colors( 0 - 255): " << StreamModifiers::endunderline
             << std::endl;
-  for (int i = 0; i < 256; ++i)
+  for (size_t i = 0; i < 256; ++i)
     std::cout << highlightString("hello world - ", i);
   std::cout << std::endl;
 } // main
