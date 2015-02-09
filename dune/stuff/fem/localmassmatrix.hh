c702821e853ea7ee8b21daa33fe633cda6477471
Felix Schindler <felix.schindler@wwu.de>
2015-02-09 12:52:35 +0100

    [fem] mark for removal, refs #37

--- dune/stuff/fem/localmassmatrix.hh
+++ dune/stuff/fem/localmassmatrix.hh
@@ -3,11 +3,14 @@
 // Copyright holders: Rene Milk, Felix Schindler
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
+#warning Will be removed soon, file an issue on https://github.com/wwu-numerik/dune-stuff/issues if you need this (09.02.2015)!
+
 #ifndef STUFF_LOCALMASSMATRIX_HH
 #define STUFF_LOCALMASSMATRIX_HH
 
 #if HAVE_DUNE_FEM
 
+#include <dune/common/deprecated.hh>
 #include <dune/common/version.hh>
 
 #include <dune/fem/operator/1order/localmassmatrix.hh>
@@ -19,7 +22,9 @@ namespace Fem {
 //! create yet another child class just in case fem actually implements something in LocalMassMatrix in addition to its
 //! base
 template <class DiscreteFunctionSpaceImp, class VolumeQuadratureImp>
-struct LocalMassMatrix : public Dune::Fem::LocalMassMatrix<DiscreteFunctionSpaceImp, VolumeQuadratureImp>
+struct DUNE_DEPRECATED_MSG("Will be removed soon, file an issue on https://github.com/wwu-numerik/dune-stuff/issues if "
+                           "you need this (09.02.2015)!") LocalMassMatrix
+    : public Dune::Fem::LocalMassMatrix<DiscreteFunctionSpaceImp, VolumeQuadratureImp>
 {
   LocalMassMatrix(const DiscreteFunctionSpaceImp& spc, const int volQuadOrd = -1)
     : Dune::Fem::LocalMassMatrix<DiscreteFunctionSpaceImp, VolumeQuadratureImp>(spc, volQuadOrd)
