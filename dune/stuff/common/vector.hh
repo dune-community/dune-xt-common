8540cea0522b21ccbcc1fba602228422e82d8a27
Rene Milk <rene.milk@uni-muenster.de>
2015-07-23 12:56:59 +0200

    [common/vector] use constexpr where possible

--- dune/stuff/common/vector.hh
+++ dune/stuff/common/vector.hh
@@ -50,11 +50,11 @@ struct VectorAbstraction
   typedef typename Dune::FieldTraits<VecType>::field_type S;
   typedef typename Dune::FieldTraits<VecType>::real_type R;
 
-  static const bool is_vector = false;
+  static constexpr bool is_vector = false;
 
-  static const bool has_static_size = false;
+  static constexpr bool has_static_size = false;
 
-  static const size_t static_size = std::numeric_limits<size_t>::max();
+  static constexpr size_t static_size = std::numeric_limits<size_t>::max();
 
   static inline /*VectorType*/ void create(const size_t /*sz*/)
   {
@@ -76,11 +76,11 @@ struct VectorAbstraction<std::vector<T>>
   typedef ScalarType S;
   typedef RealType R;
 
-  static const bool is_vector = true;
+  static constexpr bool is_vector = true;
 
-  static const bool has_static_size = false;
+  static constexpr bool has_static_size = false;
 
-  static const size_t static_size = std::numeric_limits<size_t>::max();
+  static constexpr size_t static_size = std::numeric_limits<size_t>::max();
 
   static inline VectorType create(const size_t sz)
   {
@@ -102,11 +102,11 @@ struct VectorAbstraction<Dune::DynamicVector<K>>
   typedef ScalarType S;
   typedef RealType R;
 
-  static const bool is_vector = true;
+  static constexpr bool is_vector = true;
 
-  static const bool has_static_size = false;
+  static constexpr bool has_static_size = false;
 
-  static const size_t static_size = std::numeric_limits<size_t>::max();
+  static constexpr size_t static_size = std::numeric_limits<size_t>::max();
 
   static inline VectorType create(const size_t sz)
   {
@@ -128,11 +128,11 @@ struct VectorAbstraction<Dune::FieldVector<K, SIZE>>
   typedef ScalarType S;
   typedef RealType R;
 
-  static const bool is_vector = true;
+  static constexpr bool is_vector = true;
 
-  static const bool has_static_size = true;
+  static constexpr bool has_static_size = true;
 
-  static const size_t static_size = SIZE;
+  static constexpr size_t static_size = SIZE;
 
   static inline VectorType create(const size_t sz)
   {
@@ -158,11 +158,11 @@ struct VectorAbstraction<std::complex<T>>
   typedef ScalarType S;
   typedef RealType R;
 
-  static const bool is_vector = false;
+  static constexpr bool is_vector = false;
 
-  static const bool has_static_size = true;
+  static constexpr bool has_static_size = true;
 
-  static const size_t static_size = 2u;
+  static constexpr size_t static_size = 2u;
 
   static inline VectorType create(const size_t /*sz*/)
   {
