ffd03ca070682697aac12994aa70a51966d47f7b
Rene Milk <rene.milk@wwu.de>
2015-11-10 10:37:03 +0100

    [travis] remove token from env

--- scripts/upload_test_logs.bash
+++ scripts/upload_test_logs.bash
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-git --version
-
 BRANCH=PR_${TRAVIS_PULL_REQUEST}_to_${TRAVIS_BRANCH}
 if [ "x${TRAVIS_PULL_REQUEST}" == "xfalse" ] ; then
     BRANCH=${TRAVIS_BRANCH}
@@ -15,9 +13,9 @@ DIR=${CXX_COMPILER}__wo_${MODULES_TO_DELETE// /_}
 rm -rf ${DIR}
 mkdir ${DIR}
 rsync -a ${DUNE_BUILD_DIR}/dune-stuff/dune/stuff/test/*xml ${DIR}
-printenv | sort > ${DIR}/env
+printenv | grep -v GH_TOKEN | sort > ${DIR}/env
 git add .
 git config user.name "Travis-CI"
 git config user.email "travis@travis-ci.org"
 git commit -m "Testlogs for Job ${TRAVIS_JOB_NUMBER} - ${TRAVIS_COMMIT_RANGE}"
-git push -u https://${GH_TOKEN}@github.com/wwu-numerik/dune-stuff-testlogs.git ${BRANCH} #> /dev/null 2>&1
+git push -u https://${GH_TOKEN}@github.com/wwu-numerik/dune-stuff-testlogs.git ${BRANCH} > /dev/null 2>&1
