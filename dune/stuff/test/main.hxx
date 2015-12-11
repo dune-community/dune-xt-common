15bbd37b8bcda7daa00557b3fb31cf1cfd401fe2
Tobias Leibner <tobias.leibner@uni-muenster.de>
2015-12-11 06:50:20 +0100

    [test.main] correct fix

--- dune/stuff/test/main.hxx
+++ dune/stuff/test/main.hxx
@@ -57,7 +57,7 @@ int main(int argc, char** argv)
 #endif
 
     testing::InitGoogleTest(&argc, argv);
-    if (argc > 2)
+    if (argc > 1)
       DSC_CONFIG.read_command_line(argc, argv);
 #if HAVE_DUNE_FEM
     Dune::Fem::MPIManager::initialize(argc, argv);
