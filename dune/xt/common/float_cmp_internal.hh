0d7be803e94dbf53bf323e2bb34134f0b817cf0b
Rene Milk <rene.milk@wwu.de>
2016-02-05 15:17:04 +0100

    adds dual-licensing as bsd 2 clause and gpl2+ with runtime exception

--- dune/xt/common/float_cmp_internal.hh
+++ dune/xt/common/float_cmp_internal.hh
@@ -1,7 +1,9 @@
 // This file is part of the dune-xt-common project:
 //   https://github.com/dune-community/dune-xt-common
 // The copyright lies with the authors of this file (see below).
-// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+// License: Dual licensed as  BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
 //   Barbara Verfürth (2015)
 //   Felix Schindler  (2016)
--- dune/xt/common/print.hh
+++ dune/xt/common/print.hh
@@ -1,7 +1,9 @@
 // This file is part of the dune-xt-common project:
 //   https://github.com/dune-community/dune-xt-common
 // The copyright lies with the authors of this file (see below).
-// License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+// License: Dual licensed as  BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
 // Authors:
 //   Felix Schindler (2010, 2012 - 2016)
 //   Rene Milk       (2009 - 2015)
