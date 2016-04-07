5e4a6f7d2d48df5cd4625fb226fdf1c80fcf63d6
Rene Milk <rene.milk@wwu.de>
2016-04-07 09:24:02 +0200

    DO NOT MERGE. intentionally violate clang format rules to test checker

--- dune/xt/common/float_cmp.hh
+++ dune/xt/common/float_cmp.hh
@@ -31,78 +31,78 @@ namespace FloatCmp {
 template <class T, Style style = Style::defaultStyle>
 struct DefaultEpsilon
 {
-  typedef typename Dune::FloatCmp::EpsilonType<T>::Type Type;
-  static Type value()
-  {
-    return Dune::FloatCmp::DefaultEpsilon<T, internal::ConvertStyle<style>::value>::value();
-  }
+typedef typename Dune::FloatCmp::EpsilonType<T>::Type Type;
+static Type value()
+{
+return Dune::FloatCmp::DefaultEpsilon<T, internal::ConvertStyle<style>::value>::value();
+}
 };
 
 //! since we treat complex like a vector its epsilon is (eps,eps) of its scalar type
 template <class T, Style style>
 struct DefaultEpsilon<std::complex<T>, style>
 {
-  typedef typename Dune::FloatCmp::EpsilonType<std::complex<T>>::Type Type;
-  static Type value()
-  {
-    const auto val = Dune::FloatCmp::DefaultEpsilon<T, internal::ConvertStyle<style>::value>::value();
-    return std::complex<T>(val, val);
-  }
+typedef typename Dune::FloatCmp::EpsilonType<std::complex<T>>::Type Type;
+static Type value()
+{
+const auto val = Dune::FloatCmp::DefaultEpsilon<T, internal::ConvertStyle<style>::value>::value();
+return std::complex<T>(val, val);
+}
 };
 
 template <class T>
 struct DefaultEpsilon<T, Style::numpy>
 {
-  typedef typename Dune::FloatCmp::EpsilonType<T>::Type Type;
-  static Type value()
-  {
-    return Dune::FloatCmp::DefaultEpsilon<T, Dune::FloatCmp::relativeStrong>::value();
-  }
+typedef typename Dune::FloatCmp::EpsilonType<T>::Type Type;
+static Type value()
+{
+return Dune::FloatCmp::DefaultEpsilon<T, Dune::FloatCmp::relativeStrong>::value();
+}
 };
 
 //! necessary to avoid ambig. partial specialization
 template <class T>
 struct DefaultEpsilon<std::complex<T>, Style::numpy>
 {
-  typedef typename Dune::FloatCmp::EpsilonType<std::complex<T>>::Type Type;
-  static Type value()
-  {
-    const auto val = Dune::FloatCmp::DefaultEpsilon<T, Dune::FloatCmp::relativeStrong>::value();
-    return std::complex<T>(val, val);
-  }
+typedef typename Dune::FloatCmp::EpsilonType<std::complex<T>>::Type Type;
+static Type value()
+{
+const auto val = Dune::FloatCmp::DefaultEpsilon<T, Dune::FloatCmp::relativeStrong>::value();
+return std::complex<T>(val, val);
+}
 };
 
 template <class V>
 struct MT
 {
-  typedef typename VectorAbstraction<V>::S T;
-  typedef typename Dune::FloatCmp::EpsilonType<typename MT<V>::T>::Type Eps;
+typedef typename VectorAbstraction<V>::S T;
+typedef typename Dune::FloatCmp::EpsilonType<typename MT<V>::T>::Type Eps;
 };
 
 #define DUNE_XT_COMMON_FLOAT_CMP_GENERATOR(id)                                                                         \
-  template <Style style, class FirstType, class SecondType>                                                            \
-  typename std::enable_if<internal::cmp_type_check<FirstType, SecondType, typename MT<FirstType>::T>::value,           \
-                          bool>::type                                                                                  \
-  id(const FirstType& first,                                                                                           \
-     const SecondType& second,                                                                                         \
-     const typename MT<FirstType>::Eps& rtol = DefaultEpsilon<typename MT<FirstType>::T, style>::value(),              \
-     const typename MT<FirstType>::Eps& atol = DefaultEpsilon<typename MT<FirstType>::T, style>::value())              \
-  {                                                                                                                    \
-    return internal::Call<FirstType, SecondType, typename MT<FirstType>::Eps, style>::id(first, second, rtol, atol);   \
-  }                                                                                                                    \
-                                                                                                                       \
-  template <class FirstType, class SecondType>                                                                         \
-  typename std::enable_if<internal::cmp_type_check<FirstType, SecondType, typename MT<FirstType>::T>::value,           \
-                          bool>::type                                                                                  \
-  id(const FirstType& first,                                                                                           \
-     const SecondType& second,                                                                                         \
-     const typename MT<FirstType>::Eps& rtol =                                                                         \
-         DefaultEpsilon<typename MT<FirstType>::T, Style::defaultStyle>::value(),                                      \
-     const typename MT<FirstType>::Eps& atol =                                                                         \
-         DefaultEpsilon<typename MT<FirstType>::T, Style::defaultStyle>::value())                                      \
-  {                                                                                                                    \
-    return id<Style::defaultStyle>(first, second, rtol, atol);                                                         \
-  }
+template <Style style, class FirstType, class SecondType>                                                            \
+typename std::enable_if<internal::cmp_type_check<FirstType, SecondType, typename MT<FirstType>::T>::value,           \
+bool>::type                                                                                  \
+id(const FirstType& first,                                                                                           \
+const SecondType& second,                                                                                         \
+const typename MT<FirstType>::Eps& rtol = DefaultEpsilon<typename MT<FirstType>::T, style>::value(),              \
+const typename MT<FirstType>::Eps& atol = DefaultEpsilon<typename MT<FirstType>::T, style>::value())              \
+{                                                                                                                    \
+return internal::Call<FirstType, SecondType, typename MT<FirstType>::Eps, style>::id(first, second, rtol, atol);   \
+}                                                                                                                    \
+                                                                 \
+template <class FirstType, class SecondType>                                                                         \
+typename std::enable_if<internal::cmp_type_check<FirstType, SecondType, typename MT<FirstType>::T>::value,           \
+bool>::type                                                                                  \
+id(const FirstType& first,                                                                                           \
+const SecondType& second,                                                                                         \
+const typename MT<FirstType>::Eps& rtol =                                                                         \
+DefaultEpsilon<typename MT<FirstType>::T, Style::defaultStyle>::value(),                                      \
+const typename MT<FirstType>::Eps& atol =                                                                         \
+DefaultEpsilon<typename MT<FirstType>::T, Style::defaultStyle>::value())                                      \
+{                                                                                                                    \
+return id<Style::defaultStyle>(first, second, rtol, atol);                                                         \
+}
 
 DUNE_XT_COMMON_FLOAT_CMP_GENERATOR(eq)
 DUNE_XT_COMMON_FLOAT_CMP_GENERATOR(ne)
