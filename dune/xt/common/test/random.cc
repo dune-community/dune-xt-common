e4b56f46c6243376117ead7f9c4e5d0cbe4a7c5d
Rene Milk <rene.milk@wwu.de>
2016-05-31 15:54:31 +0200

    [test/random] actual test rng assumptions

--- dune/xt/common/test/random.cc
+++ dune/xt/common/test/random.cc
@@ -28,13 +28,28 @@ TEST(Init, Random)
   typedef RNG_FIELD_TYPE T;
   typedef DefaultRNG<T> RNG;
 //  typedef typename VectorAbstraction<T>::S Scalar;
+  const auto lower_bound = init_bound<T>(1);
+  // for the int check we need at least on int in between lower and upper bound
+  const auto upper_bound = init_bound<T>(3);
+  const auto seed = init_bound<T>(0);
   RNG rng1;
-  RNG rng2(init_bound<T>(1));
-  RNG rng3(init_bound<T>(1),init_bound<T>(2));
-  RNG rng4(init_bound<T>(1),init_bound<T>(2),init_bound<T>(0));
-  for(auto& r : std::array<RNG, 4>{{rng1, rng2, rng3, rng4}}) {
-    auto DXTC_UNUSED(val) = r();
-    //!TODO check val
+  RNG rng2(lower_bound);
+  RNG rng3(lower_bound, upper_bound);
+  RNG rng4(lower_bound, upper_bound, seed);
+  for(auto& r : std::array<RNG, 3>{{rng2, rng3, rng4}}) {
+    const auto val = r();
+    EXPECT_TRUE(FloatCmp::ge(val, lower_bound));
+  }
+  for(auto& r : std::array<RNG, 2>{{rng3, rng4}}) {
+    const auto val = r();
+    EXPECT_TRUE(FloatCmp::le(val, upper_bound));
+  }
+  {
+    RNG rng_a(lower_bound, upper_bound, seed);
+    RNG rng_b(lower_bound, upper_bound, seed);
+    // same seed -> same values
+    EXPECT_EQ(rng_a(), rng_b());
+    EXPECT_EQ(rng_a(), rng_b());
   }
   DefaultRNG<std::string> str_rng(2);
   std::string rstr = str_rng();
