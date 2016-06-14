8cd9ec0915eeb82ff356126ed6697da468a9b68d
Rene Milk <rene.milk@wwu.de>
2016-06-14 14:34:11 +0200

    [cmake] only add format target if clang format 3.7 is found
    clang format discovery is improved a lot by using cmake
    builtins, allowing us to very easily target a specific
    version only.
    
    This is one half of #10

--- cmake/modules/FindClangFormat.cmake
+++ cmake/modules/FindClangFormat.cmake
@@ -5,25 +5,18 @@
 #     )                      
 #
 #   optionally pass the minimal version you require like so find_package(ClangFormat 3.7) 
-#   this module set CLANG_FORMAT_EXECUTABLE, CLANG_FORMAT_VERSION
-#   and CLANG_FORMAT_FOUND accordingly
+#   this module set ClangFormat_EXECUTABLE, ClangFormat_VERSION
+#   and ClangFormat_FOUND accordingly
 #
-set(CLANG_FORMAT_FOUND 0)
-find_program(format_binary NAMES clang-format clang-format-3.6 clang-format-3.7 clang-format-3.8 clang-format-3.9 clang-format-4.0)
-if(EXISTS ${format_binary})
-  execute_process(COMMAND ${format_binary} -version OUTPUT_VARIABLE clang_out )
-  message("clang verison out" ${clang_out})
-  string(REGEX REPLACE ".*clang-format version ([0-9]+\\.[0-9]+).*"  "\\1" CLANG_FORMAT_VERSION ${clang_out})
-  set(CLANG_FORMAT_FOUND 1)
-  set(CLANG_FORMAT_EXECUTABLE ${format_binary})
-  if(optional argument passed)
-    check match with CLANG_FORMAT_VERSION
-    if(not matched)
-      message(INFO "Requested clang-format >= arg_passed, found ${CLANG_FORMAT_VERSION}"
-      set(CLANG_FORMAT_FOUND 0)
-    endif()
-  endif()
-  #message("clang-format executable: ${CLANG_FORMAT_EXECUTABLE}")
-  #message("clang-format version: ${CLANG_FORMAT_VERSION}")
+find_program(ClangFormat_EXECUTABLE NAMES clang-format clang-format-3.6 clang-format-3.7 clang-format-3.8 clang-format-3.9 clang-format-4.0)
+if(EXISTS ${ClangFormat_EXECUTABLE})
+  execute_process(COMMAND ${ClangFormat_EXECUTABLE} -version OUTPUT_VARIABLE clang_out )
+  string(REGEX REPLACE ".*clang-format version ([0-9]+\\.[0-9]+).*"  "\\1" ClangFormat_VERSION ${clang_out})
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ClangFormat 
+    REQUIRED_VARS ClangFormat_EXECUTABLE
+    VERSION_VAR ClangFormat_VERSION
+    )
 
