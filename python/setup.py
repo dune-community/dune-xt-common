0e27482f19b0bfdf1dca181c32531e61e843564c
Rene Milk <rene.milk@wwu.de>
2016-10-27 15:04:22 +0200

    [py] create python subdir for installable module

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup
+
+setup(name='dune.xt',
+      version='2.4',
+      namespace_packages=['dune'],
+      description='Python for Dune-Xt',
+      author='The dune-xt devs',
+      author_email='dune-xt-dev@listserv.uni-muenster.de',
+      url='https://github.com/dune-community/dune-xt-common',
+      packages=['dune.xt'],
+      install_requires=['binpacking==1.3'],
+      scripts=['./scripts/generate_compare_functions.py',
+            './wrapper/dune_xt_execute.py'])
