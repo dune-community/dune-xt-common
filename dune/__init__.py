e9ae1ae777b3c339819c5748c3b421f5e233401d
Felix Schindler <felix.schindler@wwu.de>
2017-01-23 20:55:16 +0100

    drop dune/*/__init__.py, superseded by python/dune/*/__init__.py

--- dune/__init__.py
+++
@@ -1,7 +0,0 @@
-# This file is part of the dune-xt-common project:
-#   https://github.com/dune-community/dune-xt-common
-# The copyright lies with the authors of this file (see below).
-# License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
-# Authors:
-#   Felix Schindler (2016)
-
