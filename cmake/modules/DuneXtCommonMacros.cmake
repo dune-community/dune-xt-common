191810c6e07e0c3c60c4a01b4234c712fce80721
Felix Schindler <felix.schindler@wwu.de>
2017-03-07 16:47:31 +0100

    [cmake] add alberta stuff, fixes db21015053a6b55cf890b5393636b084a9f1e407

--- cmake/modules/DuneXtCommonMacros.cmake
+++ cmake/modules/DuneXtCommonMacros.cmake
@@ -61,3 +61,10 @@ if(NOT DS_HEADERCHECK_DISABLE)
 endif(NOT DS_HEADERCHECK_DISABLE)
 set( DXT_TEST_TIMEOUT 180 CACHE STRING "per-test timeout in seconds")
 set( DXT_TEST_PROCS 1   CACHE STRING "run N tests in parallel")
+
+# we inherit alberta via dune-fem -> dune-grid
+if(ALBERTA_FOUND)
+  add_definitions("-DENABLE_ALBERTA=1")
+  add_definitions("-DALBERTA_DIM=2")
+  include_directories(${ALBERTA_INCLUDES})
+endif()
