77b72b0b610a4d97ea327836d67a1227cf219d30
Felix Schindler <felix.schindler@wwu.de>
2017-03-07 21:19:43 +0100

    [config.h] guard defines to avoid warnings

--- config.h.cmake
+++ config.h.cmake
@@ -24,7 +24,9 @@
 /* Define to the revision of dune-xt-common */
 #define DUNE_XT_COMMON_VERSION_REVISION ${DUNE_XT_COMMON_VERSION_REVISION}
 
+#ifndef HAVE_EIGEN
 #define HAVE_EIGEN ENABLE_EIGEN
+#endif
 
 /* Define to 1 if threading building blocks were found, else 0 */
 #ifndef HAVE_TBB
@@ -39,92 +41,106 @@
 #define HAVE_DUNE_PYBINDXI 0
 #endif
 
-#cmakedefine HAS_WORKING_UNUSED_ATTRIBUTE  1
-
-#define HAVE_LIKWID ${HAVE_LIKWID}
-#define ENABLE_PERFMON ${ENABLE_PERFMON}
-#if ENABLE_PERFMON && HAVE_LIKWID
-# define LIKWID_PERFMON 1
+#ifndef HAS_WORKING_UNUSED_ATTRIBUTE
+#cmakedefine HAS_WORKING_UNUSED_ATTRIBUTE 1
 #endif
 
+#ifndef HAVE_LIKWID
+#define HAVE_LIKWID ${HAVE_LIKWID}
+#endif
+
+#ifndef ENABLE_PERFMON
+#define ENABLE_PERFMON ${ENABLE_PERFMON}
+#endif
+
+#if ENABLE_PERFMON && HAVE_LIKWID
+#define LIKWID_PERFMON 1
+#endif
+
+#ifndef HAVE_MAP_EMPLACE
 #cmakedefine HAVE_MAP_EMPLACE 1
+#endif
 
+#ifndef DS_MAX_MIC_THREADS
 #define DS_MAX_MIC_THREADS ${DS_MAX_MIC_THREADS}
+#endif
 
+#ifndef HAVE_DUNE_FEM_PARAMETER_REPLACE
 #define HAVE_DUNE_FEM_PARAMETER_REPLACE 0
+#endif
 
 /* needed in dune/xt/common/profiler.hh */
 #ifndef DUNE_XT_COMMON_DO_TIMING
-# define DUNE_XT_COMMON_DO_TIMING 0
+#define DUNE_XT_COMMON_DO_TIMING 0
 #endif
 
 /*** Silence implicitly False evaluation of undefined macro warnings ****/
 #ifndef HAVE_DUNE_FEM
-# define HAVE_DUNE_FEM 0
+#define HAVE_DUNE_FEM 0
 #endif
 
 #ifndef HAVE_DUNE_GRID_MULTISCALE
-# define HAVE_DUNE_GRID_MULTISCALE 0
+#define HAVE_DUNE_GRID_MULTISCALE 0
 #endif
 
 #ifndef HAVE_ALBERTA
-# define HAVE_ALBERTA 0
+#define HAVE_ALBERTA 0
 #endif
 
 #ifndef ALBERTA_DIM
-# define ALBERTA_DIM 2
+#define ALBERTA_DIM 2
 #endif
 
 #ifndef HAVE_DUNE_SPGRID
-# define HAVE_DUNE_SPGRID 0
+#define HAVE_DUNE_SPGRID 0
 #endif
 
 #ifndef HAVE_EMPLACE
-# define HAVE_EMPLACE 0
+#define HAVE_EMPLACE 0
 #endif
 
 #ifndef ENABLE_SUPERLU
-# define ENABLE_SUPERLU 0
+#define ENABLE_SUPERLU 0
 #endif
 
 #ifndef HAVE_SUPERLU
-# define HAVE_SUPERLU 0
+#define HAVE_SUPERLU 0
 #endif
 
 #ifndef ENABLE_UMFPACK
-# define ENABLE_UMFPACK 0
+#define ENABLE_UMFPACK 0
 #endif
 
 #ifndef HAVE_UMFPACK
-# define HAVE_UMFPACK 0
+#define HAVE_UMFPACK 0
 #endif
 
 #ifndef ENABLE_BOOST
-# define ENABLE_BOOST 0
+#define ENABLE_BOOST 0
 #endif
 
 #ifndef ENABLE_PETSC
-# define ENABLE_PETSC 0
+#define ENABLE_PETSC 0
 #endif
 
 #ifndef ENABLE_MPI
-# define ENABLE_MPI 0
+#define ENABLE_MPI 0
 #endif
 
 #ifndef HAVE_MPI
-# define HAVE_MPI 0
+#define HAVE_MPI 0
 #endif
 
 #ifndef HAVE_CONSTEXPR
-# define HAVE_CONSTEXPR 0
+#define HAVE_CONSTEXPR 0
 #endif
 
 #ifndef HAVE_PETSC
-# define HAVE_PETSC 0
+#define HAVE_PETSC 0
 #endif
 
 #ifndef DUNE_FEM_COMPATIBILITY
-# define DUNE_FEM_COMPATIBILITY 0
+#define DUNE_FEM_COMPATIBILITY 0
 #endif
 
 #ifndef HAVE_MAP_EMPLACE
