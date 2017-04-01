a55ba7b42aa7de4d0667f4c2f6d588c05e66bf4f
Felix Schindler <felix.schindler@wwu.de>
2017-04-01 09:44:13 +0200

    [disable_warnings] sort alphabetically

--- dune/xt/common/disable_warnings.hh
+++ dune/xt/common/disable_warnings.hh
@@ -12,6 +12,7 @@
 
 #include <boost/config.hpp>
 
+// Please sort these alphabetically!
 #if defined(BOOST_CLANG) && BOOST_CLANG
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
@@ -28,6 +29,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
 #pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wreorder"
 #pragma GCC diagnostic ignored "-Wreturn-stack-address"
 #pragma GCC diagnostic ignored "-Wshadow"
@@ -41,21 +43,21 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-private-field"
 #pragma GCC diagnostic ignored "-Wused-but-marked-unused"
-#pragma GCC diagnostic ignored "-Wpedantic"
 #elif defined(BOOST_GCC) && BOOST_GCC
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
-#if (__GNUC__ > 5)
-#pragma GCC diagnostic ignored "-Wmisleading-indentation"
-#endif
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #pragma GCC diagnostic ignored "-Wextra"
 #pragma GCC diagnostic ignored "-Wfloat-equal"
 #pragma GCC diagnostic ignored "-Wignored-qualifiers"
 #pragma GCC diagnostic ignored "-Wlogical-op"
+#if (__GNUC__ > 5)
+#pragma GCC diagnostic ignored "-Wmisleading-indentation"
+#endif
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
 #pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wreorder"
 #pragma GCC diagnostic ignored "-Wshadow"
 #pragma GCC diagnostic ignored "-Wswitch-enum"
@@ -65,5 +67,4 @@
 #pragma GCC diagnostic ignored "-Wunreachable-code"
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wpedantic"
 #endif
