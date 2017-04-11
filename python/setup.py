cc42c3c9fe27f0d8985af85c1b3befb98d8d6a9c
Rene Milk <rene.milk@wwu.de>
2017-04-11 16:30:09 +0200

    [python] add script to run speedup bench on numa nodes

--- python/setup.py
+++ python/setup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+#
+# This file is part of the dune-xt-common project:
+#   https://github.com/dune-community/dune-xt-common
+# Copyright 2009-2017 dune-xt-common developers and contributors. All rights reserved.
+# License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+#      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+#          with "runtime exception" (http://www.dune-project.org/license.html)
+# Authors:
+#   Felix Schindler (2017)
+#   Rene Milk       (2016 - 2017)
 
 import sys
 from setuptools import setup
@@ -11,7 +21,8 @@ setup(name='dune.xt',
       author_email='dune-xt-dev@listserv.uni-muenster.de',
       url='https://github.com/dune-community/dune-xt-common',
       packages=['dune.xt'],
-      install_requires=['binpacking==1.3'],
+      install_requires=['binpacking==1.3', 'cython', 'numa', 'docopt'],
       scripts=['./scripts/generate_compare_functions.py',
                './scripts/distribute_testing.py',
+               './scripts/numa_speedup.py',
                './wrapper/dune_xt_execute.py'])
