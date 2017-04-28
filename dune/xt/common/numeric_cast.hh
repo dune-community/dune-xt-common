57309b8d5b4f54269e68ec5dc419d3fbec2686d9
Felix Schindler <felix.schindler@wwu.de>
2017-04-28 17:55:01 +0200

    [numeric_cast] add numeric_cast<target>(source)

---
+++ dune/xt/common/numeric_cast.hh
@@ -0,0 +1,39 @@
+#ifndef DUNE_XT_COMMON_NUMERIC_CAST_HH
+#define DUNE_XT_COMMON_NUMERIC_CAST_HH
+
+#include <boost/numeric/conversion/cast.hpp>
+
+#include "exceptions.hh"
+#include "type_traits.hh"
+
+namespace Dune {
+namespace XT {
+namespace Common {
+
+
+/**
+ * \brief Calls boost::numeric_cast and gives some information on failure.
+ */
+template <class T, class S>
+T numeric_cast(S source)
+{
+  T target;
+  try {
+    target = boost::numeric_cast<size_t>(source);
+  } catch (boost::bad_numeric_cast& ee) {
+    DUNE_THROW(XT::Common::Exceptions::external_error,
+               "  The following error occured in boost while converting '" << source << "' to '"
+                                                                           << XT::Common::Typename<size_t>::value()
+                                                                           << ":\n\n"
+                                                                           << ee.what());
+  }
+  return target;
+} // ... numeric_cast(...)
+
+
+} // namespace Common
+} // namespace XT
+} // namespace Dune
+
+
+#endif // DUNE_XT_COMMON_NUMERIC_CAST_HH
