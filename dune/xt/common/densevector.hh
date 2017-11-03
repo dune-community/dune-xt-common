149a7b3bdfc80baaf58db1abe579d19aea3f6fec
Felix Schindler <felix.schindler@wwu.de>
2017-11-03 13:47:28 +0100

    [densevector] add operator{*,/} for all Dune::DenseVector<>s

---
+++ dune/xt/common/densevector.hh
@@ -0,0 +1,52 @@
+// This file is part of the dune-xt-common project:
+//   https://github.com/dune-community/dune-xt-common
+// Copyright 2009-2017 dune-xt-common developers and contributors. All rights reserved.
+// License: Dual licensed as BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
+//      or  GPL-2.0+ (http://opensource.org/licenses/gpl-license)
+//          with "runtime exception" (http://www.dune-project.org/license.html)
+// Authors:
+//   Felix Schindler  (2017)
+
+#ifndef DUNE_XT_COMMON_DENSEVECTOR_HH
+#define DUNE_XT_COMMON_DENSEVECTOR_HH
+
+#include <dune/common/densevector.hh>
+
+#include <dune/xt/common/type_traits.hh>
+
+namespace Dune {
+
+
+template <class S, class V>
+typename std::enable_if<Dune::XT::Common::is_arithmetic<S>::value, typename DenseVector<V>::derived_type>::type
+operator*(const S& alpha, const DenseVector<V>& vec)
+{
+  typename DenseVector<V>::derived_type ret = vec;
+  ret *= alpha;
+  return ret;
+}
+
+
+template <class V, class S>
+typename std::enable_if<Dune::XT::Common::is_arithmetic<S>::value, typename DenseVector<V>::derived_type>::type
+operator*(const DenseVector<V>& vec, const S& alpha)
+{
+  typename DenseVector<V>::derived_type ret = vec;
+  ret *= alpha;
+  return ret;
+}
+
+
+template <class V, class S>
+typename std::enable_if<Dune::XT::Common::is_arithmetic<S>::value, typename DenseVector<V>::derived_type>::type
+operator/(const DenseVector<V>& vec, const S& alpha)
+{
+  typename DenseVector<V>::derived_type ret = vec;
+  ret /= alpha;
+  return ret;
+}
+
+
+} // namespace Dune
+
+#endif // DUNE_XT_COMMON_DENSEVECTOR_HH
