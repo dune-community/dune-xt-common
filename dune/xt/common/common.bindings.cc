71921cab485f6df9ef214b2c9e2005da63440683
Felix Schindler <felix.schindler@wwu.de>
2017-12-02 09:17:54 +0100

    [bindings|common] use init macro for module

--- dune/xt/common/common.bindings.cc
+++ dune/xt/common/common.bindings.cc
@@ -11,73 +11,13 @@
 
 #if HAVE_DUNE_PYBINDXI
 
-#include <string>
-#include <vector>
-
-#include <dune/common/parallel/mpihelper.hh>
-
-#if HAVE_DUNE_FEM
-#include <dune/fem/misc/mpimanager.hh>
-#endif
-
-#include <dune/pybindxi/pybind11.h>
-#include <dune/pybindxi/stl.h>
-
 #include "bindings.hh"
 
 
 PYBIND11_PLUGIN(_common)
 {
-  namespace py = pybind11;
-  using namespace pybind11::literals;
-
-  py::module m("_common", "dune-xt-common");
-
-  Dune::XT::Common::bindings::addbind_exceptions(m);
-
-  m.def("_init_mpi",
-        [](const std::vector<std::string>& args) {
-          int argc = Dune::XT::Common::numeric_cast<int>(args.size());
-          char** argv = Dune::XT::Common::vector_to_main_args(args);
-          Dune::MPIHelper::instance(argc, argv);
-#if HAVE_DUNE_FEM
-          Dune::Fem::MPIManager::initialize(argc, argv);
-#endif
-        },
-        "args"_a = std::vector<std::string>());
-
-  m.def("_init_logger",
-        [](const ssize_t max_info_level,
-           const ssize_t max_debug_level,
-           const bool enable_warnings,
-           const bool enable_colors,
-           const std::string& info_color,
-           const std::string& debug_color,
-           const std::string& warning_color) {
-          Dune::XT::Common::TimedLogger().create(
-              max_info_level, max_debug_level, enable_warnings, enable_colors, info_color, debug_color, warning_color);
-        },
-        "max_info_level"_a = std::numeric_limits<ssize_t>::max(),
-        "max_debug_level"_a = std::numeric_limits<ssize_t>::max(),
-        "enable_warnings"_a = true,
-        "enable_colors"_a = true,
-        "info_color"_a = "blue",
-        "debug_color"_a = "darkgray",
-        "warning_color"_a = "red");
-
-  m.def("_test_logger",
-        [](const bool info, const bool debug, const bool warning) {
-          auto logger = Dune::XT::Common::TimedLogger().get("dune.xt.common");
-          if (info)
-            logger.info() << "info logging works!" << std::endl;
-          if (debug)
-            logger.debug() << "debug logging works!" << std::endl;
-          if (warning)
-            logger.warn() << "warning logging works!" << std::endl;
-        },
-        "info"_a = true,
-        "debug"_a = true,
-        "warning"_a = true);
+  pybind11::module m("_common", "dune-xt-common");
+  DUNE_XT_COMMON_BINDINGS_INITIALIZE(m, "dune.xt.common");
 
   m.def("_is_debug", []() {
 #ifndef NDEBUG
@@ -89,5 +29,6 @@ PYBIND11_PLUGIN(_common)
 
   return m.ptr();
 }
+
 
 #endif // HAVE_DUNE_PYBINDXI
