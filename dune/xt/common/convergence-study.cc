17fab335f1d241fbada83f9df80b94db6aa60e87
Felix Schindler <felix.schindler@wwu.de>
2018-08-05 00:00:05 +0200

    [convergence-study] modernize and complete rewrite

--- dune/xt/common/convergence-study.cc
+++ dune/xt/common/convergence-study.cc
@@ -9,190 +9,278 @@
 //   Rene Milk       (2015 - 2016, 2018)
 
 #include "config.h"
-#include "convergence-study.hh"
 
 #include <algorithm>
-#include <boost/io/ios_state.hpp>
 
 #include <dune/xt/common/exceptions.hh>
+#include <dune/xt/common/float_cmp.hh>
 #include <dune/xt/common/string.hh>
 #include <dune/xt/common/color.hh>
+
+#include "convergence-study.hh"
 
 namespace Dune {
 namespace XT {
 namespace Common {
 
-ConvergenceStudy::ConvergenceStudy(const std::vector<std::string> only_these_norms)
-  : only_these_norms_(only_these_norms)
+
+double ConvergenceStudy::expected_rate(const std::string& /*type*/, const std::string& /*id*/) const
 {
+  return 1;
 }
 
-std::vector<std::string> ConvergenceStudy::used_norms() const
+std::vector<std::string> ConvergenceStudy::filter(const std::vector<std::string>& vec,
+                                                  const std::vector<std::string>& only_these) const
 {
-  if (only_these_norms_.empty())
-    return provided_norms();
+  if (only_these.empty())
+    return vec;
   else {
     std::vector<std::string> ret;
-    for (auto norm : provided_norms())
-      if (std::find(only_these_norms_.begin(), only_these_norms_.end(), norm) != only_these_norms_.end())
-        ret.push_back(norm);
+    for (const auto& element : vec) {
+      if (std::find(only_these.begin(), only_these.end(), element) != only_these.end())
+        ret.emplace_back(element);
+    }
     return ret;
   }
-} // ... used_norms(...)
+} // ... filter(...)
 
-std::map<std::string, std::vector<double>>
-ConvergenceStudy::run(const bool relative, std::ostream& out, const bool print_timings)
+std::string ConvergenceStudy::lfill(const std::string& id, const size_t len) const
 {
-  boost::io::ios_all_saver guard(out);
-  if (provided_norms().size() == 0)
-    DUNE_THROW(Dune::InvalidStateException, "You have to provide at least one norm!");
-  const auto actually_used_norms = used_norms();
-  if (actually_used_norms.size() == 0)
-    DUNE_THROW(Dune::InvalidStateException, "There are no common norms in 'provided_norms()' and 'only_these_norms'!");
+  if (id.size() == len)
+    return id;
+  else if (id.size() > len)
+    return id.substr(0, len - 1) + ".";
+  else /*if (id.size() < len_)*/
+    return std::string(len - id.size(), ' ') + id;
+}
 
-  std::map<std::string, std::vector<double>> ret;
-  for (const auto& norm : actually_used_norms)
-    ret[norm] = std::vector<double>();
-
-  // print table header
-  out << identifier() << std::endl;
-  if (identifier().size() > 22 * (actually_used_norms.size() + 1))
-    out << whitespaceify(identifier(), '=') << std::endl;
+std::string ConvergenceStudy::cfill(const std::string& id, const size_t len) const
+{
+  if (id.size() > len)
+    return id.substr(0, len - 1) + ".";
   else {
-    out << "=====================";
-    for (size_t nn = 0; nn < actually_used_norms.size(); ++nn)
-      out << "======================";
+    const size_t rpadd = std::ceil((len - id.size()) / 2);
+    return std::string(len - rpadd - id.size(), ' ') + id + std::string(rpadd, ' ');
   }
-  out << "\n";
-  // * print line of grid and norms
-  out << "        grid         ";
-  for (const auto& norm : actually_used_norms) {
-    std::string relative_norm_str = "";
-    if (norm.empty()) {
-      relative_norm_str = "???";
-      if (relative)
-        relative_norm_str += " (relative)";
-      else
-        relative_norm_str += " (absolute)";
-    } else if (norm.size() <= 8) {
-      relative_norm_str = norm;
-      if (relative)
-        relative_norm_str += " (relative)";
-      else
-        relative_norm_str += " (absolute)";
-    } else if (norm.size() <= 12) {
-      relative_norm_str = norm;
-      if (relative)
-        relative_norm_str += " (rel.)";
-      else
-        relative_norm_str += " (abs.)";
-    } else {
-      if (relative)
-        relative_norm_str = norm.substr(0, 11) + ". (rel.)";
-      else
-        relative_norm_str = norm.substr(0, 19);
-    }
-    const double missing = (19.0 - relative_norm_str.size()) / 2.0;
-    for (size_t ii = 0; ii < missing; ++ii)
-      relative_norm_str += " ";
-    DXT_ASSERT(relative_norm_str.size() <= 19);
-    out << "| " << std::setw(19) << relative_norm_str << " ";
-  }
-  out << "\n";
-  // * print thin delimiter
-  out << "---------------------";
-  for (size_t nn = 0; nn < actually_used_norms.size(); ++nn)
-    out << "+---------------------";
-  out << "\n";
-  // * print size/width/error/EOC markers
-  out << "     size |    width ";
-  for (size_t nn = 0; nn < actually_used_norms.size(); ++nn)
-    out << "|    error |      EOC ";
-  out << "\n";
-  // * print thick delimiter
-  out << "==========+==========";
-  for (size_t nn = 0; nn < actually_used_norms.size(); ++nn)
-    out << "+==========+==========";
-  out << std::endl;
+}
 
-  // prepare data structures
-  std::map<std::string, double> reference_norm;
-  std::map<std::string, double> last_relative_error;
-  for (const auto& norm : actually_used_norms) {
-    if (relative)
-      reference_norm[norm] = norm_reference_solution(norm);
-    else
-      reference_norm[norm] = 0.0;
-    last_relative_error[norm] = 0.0;
-  }
-  double last_grid_width = current_grid_width();
-
-  // iterate
-  for (size_t ii = 0; ii <= num_refinements(); ++ii) {
-    // print delimiter
-    if (ii > 0) {
-      out << "----------+----------";
-      for (size_t nn = 0; nn < actually_used_norms.size(); ++nn)
-        out << "+----------+----------";
-      out << "\n";
-    }
-
-    // print grid size
-    out << " " << std::setw(8) << current_grid_size() << std::flush;
-    // print grid with
-    out << " | " << std::setw(8) << std::setprecision(2) << std::scientific << current_grid_width() << std::flush;
-
-    // do the computation
-    const double elapsed = compute_on_current_refinement();
-
-    // loop over all norms/columns
-    for (const auto& norm : actually_used_norms) {
-      // compute and print relative error
-      double relative_error = current_error_norm(norm);
-      if (relative)
-        relative_error /= reference_norm[norm];
-      ret[norm].push_back(relative_error);
-      out << " | " << std::setw(8) << std::setprecision(2) << std::scientific << relative_error << std::flush;
-      // print eoc
-      out << " | ";
-      if (ii == 0)
-        out << std::setw(8) << "----" << std::flush;
-      else {
-        const double eoc_value =
-            std::log(relative_error / last_relative_error[norm]) / std::log(current_grid_width() / last_grid_width);
-        std::stringstream eoc_string;
-        eoc_string << std::setw(8) << std::setprecision(2) << std::fixed << eoc_value;
-        if (eoc_value > (0.9 * expected_rate(norm)))
-          out << color_string(eoc_string.str(), Colors::green) << std::flush;
-        else if (eoc_value > 0.0)
-          out << color_string(eoc_string.str(), Colors::brown) << std::flush;
-        else
-          out << color_string(eoc_string.str(), Colors::red) << std::flush;
+double ConvergenceStudy::extract(const std::map<size_t, std::map<std::string, std::map<std::string, double>>>& data,
+                                 const size_t level,
+                                 const std::string& type,
+                                 const std::string& id) const
+{
+  try {
+    const auto& level_data = data.at(level);
+    try {
+      const auto& type_data = level_data.at(type);
+      try {
+        return type_data.at(id);
+      } catch (const std::out_of_range&) {
+        DUNE_THROW(Exceptions::requirements_not_met,
+                   "data missing for\n   level = " << level << "\n   type = " << type << "\n   id = " << id);
       }
-      // update
-      last_relative_error[norm] = relative_error;
-    } // loop over all norms/columns
-    // print time
-    if (print_timings) {
-      if (elapsed < 1.0)
-        out << "  (solve took " << ssize_t(1000 * elapsed) << "ms)";
-      else
-        out << "  (solve took " << std::setprecision(2) << std::fixed << elapsed << "s)";
+    } catch (const std::out_of_range&) {
+      DUNE_THROW(Exceptions::requirements_not_met, "data missing for\n   level = " << level << "\n   type = " << type);
     }
-    out << std::endl;
-    // update
-    last_grid_width = current_grid_width();
-    if (ii < num_refinements())
-      refine();
-  } // iterate
+  } catch (const std::out_of_range&) {
+    DUNE_THROW(Exceptions::requirements_not_met, "data missing for level " << level << "!");
+  }
+} // ... extract(...)
 
-  return ret;
+void ConvergenceStudy::print_eoc(std::ostream& out,
+                                 const size_t len,
+                                 const std::map<size_t, std::map<std::string, std::map<std::string, double>>>& data,
+                                 const size_t level,
+                                 const std::string& type,
+                                 const std::string& id,
+                                 const std::string& target_id) const
+{
+  auto& self = *this;
+  const double quantity_old = extract(data, level - 1, type, id);
+  if (FloatCmp::eq(quantity_old, 0.))
+    out << lfill("inf", len);
+  else {
+    const double quantity_new = extract(data, level, type, id);
+    const auto target_old = extract(data, level - 1, "target", target_id);
+    const auto target_new = extract(data, level, "target", target_id);
+    const double eoc_value = std::log(quantity_new / quantity_old) / std::log(target_new / target_old);
+    std::stringstream eoc_str;
+    if (eoc_value < -999)
+      eoc_str << "-inf";
+    else if (eoc_value > 9999)
+      eoc_str << "inf";
+    else
+      eoc_str << std::setprecision(len - /*dot*/ 1 - /*sign*/ (eoc_value > 0 ? 0 : 1)
+                                   - /*prefix*/ (std::ceil(std::abs(std::log10(std::abs(eoc_value))))))
+              << std::fixed << eoc_value;
+    // color string
+    if (eoc_value > (0.9 * self.expected_rate(type, id)))
+      out << color_string(lfill(eoc_str.str(), len), Colors::green);
+    else if (eoc_value > 0.0)
+      out << color_string(lfill(eoc_str.str(), len), Colors::brown);
+    else
+      out << color_string(lfill(eoc_str.str(), len), Colors::red);
+  }
+} // ... print_eoc(...)
+
+void ConvergenceStudy::run(const std::vector<std::string>& only_these, std::ostream& out)
+{
+  auto& self = *this;
+  // check what we want to compute and print
+  DUNE_THROW_IF(self.targets().empty(),
+                Exceptions::you_are_using_this_wrong,
+                "there has to be at least a single target!\n   self.targets() = " << self.targets());
+  const std::vector<std::string> actual_targets =
+      filter(self.targets(), only_these).empty() ? self.targets() : filter(self.targets(), only_these);
+  const std::vector<std::string> actual_norms = filter(self.norms(), only_these);
+  //  const std::vector<std::string, std::string> actual_estimates = filter(self.estimates(), only_these);
+  const std::vector<std::string> actual_quantities = filter(self.quantities(), only_these);
+  DUNE_THROW_IF(actual_norms.size() + actual_quantities.size() == 0,
+                Exceptions::you_are_using_this_wrong,
+                "there has to be at least a single norm estimate, or quantity!\n   self.norms() = "
+                    << self.norms()
+                    //                    << "\n   self.estimates() = "
+                    //                    << self.estimates()
+                    << "\n   self.quantities() = "
+                    << self.quantities()
+                    << "\n   only_these = "
+                    << only_these);
+  const size_t column_width = 8;
+  const size_t eoc_column_width = (actual_targets.size() > 1) ? 8 : 4;
+  // build header
+  const auto disc_info_title = self.discretization_info_title();
+  // - discretization
+  std::string h1 = " ";
+  std::string d1(disc_info_title.size() + 2, '-');
+  std::string h2 = " " + disc_info_title + " ";
+  std::string delim = d1;
+  for (const auto& id : actual_targets) {
+    h2 += "| " + lfill(id, column_width) + " ";
+    d1 += std::string(column_width + 3, '-');
+    delim += "+" + std::string(column_width + 2, '-');
+  }
+  h1 += cfill("discretization", disc_info_title.size() + actual_targets.size() * (column_width + 3)) + " ";
+  // - norms
+  for (const auto& id : actual_norms) {
+    h1 += "| " + cfill(id, column_width + actual_targets.size() * (eoc_column_width + 3)) + " ";
+    d1 += "+" + std::string(column_width + 2 + actual_targets.size() * (eoc_column_width + 3), '-');
+    h2 += "| " + lfill("value", column_width) + " ";
+    delim += "+" + std::string(column_width + 2, '-');
+    for (const auto& target_id : actual_targets) {
+      h2 += "| " + lfill(actual_targets.size() == 1 ? "EOC" : "EOC(" + target_id + ")", eoc_column_width) + " ";
+      delim += "+" + std::string(eoc_column_width + 2, '-');
+    }
+  }
+// - estimates
+#if 0
+    for id, _ in actual_estimates:
+        if print_full_estimate:
+            h1 += "| " + cfill(id, column_width + (len(actual_targets) + 1)*(eoc_column_width + 3)) + " "
+        else:
+            h1 += "| " + cfill(id, (len(actual_targets) + 1)*(eoc_column_width + 3) - 3) + " "
+        if print_full_estimate:
+            d1 += "+" + "-"*(column_width + 2)
+            h2 += "| " + lfill("estimate", column_width) + " "
+            delim += "+" + "-"*(column_width + 2)
+        d1 += "+" + "-"*(eoc_column_width + 2)
+        h2 += "| " + lfill("eff.", eoc_column_width) + " "
+        delim += "+" + "-"*(eoc_column_width + 2)
+        for target_id in actual_targets:
+            d1 += "+" + "-"*(eoc_column_width + 2)
+            h2 += "| " + lfill("EOC" if len(actual_targets) == 1 else "EOC ({})".format(target_id), eoc_column_width) + " "
+            delim += "+" + "-"*(eoc_column_width + 2)
+#endif // 0
+  // - quantities
+  for (const auto& id : actual_quantities) {
+    if (id.size() <= column_width) {
+      h1 += "| " + cfill(id, column_width) + " ";
+      d1 += "+" + std::string(column_width + 2, ' ');
+      h2 += "| " + std::string(column_width + 2, ' ');
+    } else if (id.size() <= 2 * column_width) {
+      h1 += "| " + id.substr(0, column_width) + " ";
+      d1 += "+ " + cfill(id.substr(column_width, column_width), column_width) + " ";
+      h2 += "| " + std::string(column_width + 2, ' ');
+    } else {
+      h1 += "| " + id.substr(0, column_width) + " ";
+      d1 += "+ " + cfill(id.substr(column_width, column_width), column_width) + " ";
+      h2 += "| " + cfill(id.substr(2 * column_width), column_width) + " ";
+    }
+    delim += "+" + std::string(column_width + 2, '-');
+  }
+  // print header
+  std::string tmp = delim;
+  std::replace(tmp.begin(), tmp.end(), '-', '=');
+  out << std::string(h1.size(), '=') << "\n" << h1 << "\n" << d1 << "\n" << h2 << "\n" << tmp << std::endl;
+  std::map<size_t, std::map<std::string, std::map<std::string, double>>> data;
+  // run actual study
+  for (size_t level = 0; level <= self.num_refinements(); ++level) {
+    // compute some discretization statistics
+    const auto disc_info = self.discretization_info(level);
+    // and print them
+    out << " " << cfill(disc_info, disc_info_title.size()) << " " << std::flush;
+    // do the actual computation
+    data[level] = compute(level, only_these);
+    // and print the results
+    // - targets
+    for (const auto& id : actual_targets) {
+      std::stringstream ss;
+      ss << std::setprecision(2) << std::scientific << extract(data, level, "target", id);
+      out << "| " << lfill(ss.str(), column_width) << " " << std::flush;
+    }
+    // - norms
+    for (const auto& id : actual_norms) {
+      std::stringstream ss;
+      ss << std::setprecision(2) << std::scientific << extract(data, level, "norm", id);
+      out << "| " << lfill(ss.str(), column_width) << " " << std::flush;
+      for (const auto& target_id : actual_targets) {
+        if (level == 0)
+          out << "| " << lfill("----", eoc_column_width) << " " << std::flush;
+        else {
+          out << "| ";
+          print_eoc(out, eoc_column_width, data, level, "norm", id, target_id);
+          out << " " << std::flush;
+        }
+      }
+    }
+// - estimates
+#if 0
+     if not "estimate" in self.data[level]:
+         self.data[level]["estimate"] = {}
+     for estimate_id, norm_id in actual_estimates:
+         self.data[level]["estimate"][estimate_id] = self.compute_estimate(level, estimate_id)
+         if not norm_id in self.data[level]["norm"]:
+             self.data[level]["norm"][norm_id] = self.compute_norm(level, norm_id)
+         if print_full_estimate:
+             print("| " + lfill("{:.2e}".format(self.data[level]["estimate"][estimate_id]), column_width) + " ", end="")
+             sys.stdout.flush()
+         print("| " +
+                 lfill("{:.2f}".format(self.data[level]["norm"][norm_id]/self.data[level]["estimate"][estimate_id]),
+                     eoc_column_width) + " ", end="")
+         sys.stdout.flush()
+         for target_id in actual_targets:
+             if level == 0:
+                 print("| " + lfill("----", eoc_column_width) + " ", end="")
+                 sys.stdout.flush()
+             else:
+                 print("| " + print_eoc(self.data[level - 1]["estimate"][estimate_id],
+                                          self.data[level]["estimate"][estimate_id],
+                                          level, target_id) + " ", end="")
+                 sys.stdout.flush()
+#endif // 0
+    // - quantities
+    for (const auto& id : actual_quantities) {
+      std::stringstream ss;
+      ss << std::setprecision(2) << std::scientific << extract(data, level, "quantity", id);
+      out << "| " << lfill(ss.str(), column_width) << " " << std::flush;
+    }
+    // end of line
+    out << " (solve took " << extract(data, level, "info", "time_to_solution") << "s)\n";
+    if (level < self.num_refinements())
+      out << delim;
+    out << std::endl;
+  }
 } // ... run(...)
 
-std::vector<double> ConvergenceStudy::expected_results(const std::string /*type*/) const
-{
-  DUNE_THROW(Exceptions::you_have_to_implement_this, "If you want to use this within the test suite!");
-}
 
 } // namespace Common
 } // namespace XT
