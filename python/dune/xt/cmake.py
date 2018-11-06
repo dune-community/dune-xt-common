289f9a4a7604d66045dc256340b57c1a1ac07d40
Tobias Leibner <tobias.leibner@googlemail.com>
2018-11-06 20:44:56 +0100

    [cmake.py] add / to word characters

--- python/dune/xt/cmake.py
+++ python/dune/xt/cmake.py
@@ -18,7 +18,7 @@ def parse_cache(filepath):
     import pyparsing as p
     EQ = p.Literal('=').suppress()
     DP = p.Literal(':').suppress()
-    token = p.Word(p.alphanums+'_- .')
+    token = p.Word(p.alphanums+'/_- .')
     line = p.Group(token('key') + DP + token('type') + EQ + p.restOfLine('value'))
     line.ignore(p.pythonStyleComment)
     line.ignore(p.dblSlashComment)
