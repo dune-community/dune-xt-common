39ca84eac93d38e76a1aa42489fc20f6e1cc6531
Tobias Leibner <tobias.leibner@googlemail.com>
2018-11-13 00:38:04 +0100

    [cmake.FindMPI4PY] also search in dune-env

--- cmake/modules/FindMPI4PY.cmake
+++ cmake/modules/FindMPI4PY.cmake
@@ -16,10 +16,16 @@
 # 1a660df142e9a
 
 if(NOT MPI4PY_INCLUDE_DIR)
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c" "import mpi4py; print(mpi4py.get_include())"
+	execute_process(COMMAND "${DUNE_PYTHON_VIRTUALENV_EXECUTABLE}" "-c" "import mpi4py; print(mpi4py.get_include())"
                   OUTPUT_VARIABLE MPI4PY_INCLUDE_DIR
                   RESULT_VARIABLE MPI4PY_COMMAND_RESULT
                   OUTPUT_STRIP_TRAILING_WHITESPACE)
+  if(MPI4PY_COMMAND_RESULT)
+	execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c" "import mpi4py; print(mpi4py.get_include())"
+                  OUTPUT_VARIABLE MPI4PY_INCLUDE_DIR
+                  RESULT_VARIABLE MPI4PY_COMMAND_RESULT
+                  OUTPUT_STRIP_TRAILING_WHITESPACE)
+  endif()
   if(MPI4PY_COMMAND_RESULT)
     message("jfa: mpi4py not found")
     set(MPI4PY_FOUND FALSE)
